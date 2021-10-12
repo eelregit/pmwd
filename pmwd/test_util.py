10bdc9a183c8d307f03f5a94ea35065c5a74265e
Yin Li <eelregit@gmail.com>
2021-10-12 19:19:34 -0400

    Add float0, & cot & cot_out skipping to check_custom_vjp

--- pmwd/test_util.py
+++ pmwd/test_util.py
@@ -1,40 +1,71 @@
 from functools import partial
 
+import numpy as np
+import jax.numpy as jnp
 import jax.test_util as jtu
-
-from jax import vjp
+from jax import vjp, float0
 from jax import random
 from jax.tree_util import tree_map
 
 
-def randn_like(key, x):
-    return random.normal(key, shape=x.shape, dtype=x.dtype)
+def randn_float0_like(x, mean=0., std=1., key=random.PRNGKey(0)):
+    if issubclass(x.dtype.type, (jnp.bool_, jnp.integer)):
+        # FIXME after https://github.com/google/jax/issues/4433 is addressed
+        return np.empty(x.shape, dtype=float0)
+    return mean + std * random.normal(key, shape=x.shape, dtype=x.dtype)
 
 
-def check_custom_vjp(fun, primals, args=(), kwargs={}, atol=None, rtol=None):
+def tree_randn_float0_like(tree, mean=None, std=None):
+    if mean is None:
+        mean = tree_map(lambda x: 0., tree)
+    if std is None:
+        std = tree_map(lambda x: 1., tree)
+    return tree_map(randn_float0_like, tree, mean, std)
+
+
+def tree_zero_leaves(tree, mask=None):
+    """Replace leaves of a pytree with 0's given a mask
+    """
+    if mask is None:
+        return tree
+    return tree_map(lambda x, skip: 0 if skip else x, tree, mask)
+
+
+def check_custom_vjp(
+        fun, primals,
+        cot_out_mean=None, cot_out_std=None, cot_skip=None,
+        args=(), kwargs={}, atol=None, rtol=None):
     """Compare custom and automatic vjp's of a decorated function.
 
     Parameters:
         fun: function decorated with `custom_vjp`
         primals: function inputs whose cotangent vectors are to be compared
-        args: positional function inputs to be fixed by `partial`
-        kwargs: keyword function inputs to be fixed by `partial`
+        cot_out_mean: mean of randn output cotangents. Default is a pytree of 0
+        cot_out_std: std of randn output cotangents. Default is a pytree of 1
+        cot_skip: ignore some cotangent comparisons. Default is to skip none
+        args: positional inputs to be fixed by `partial`
+        kwargs: keyword inputs to be fixed by `partial`
         atol: absolute tolerance
         rtol: relative tolerance
-    """
-    key = random.PRNGKey(0)
 
+    Note:
+        Setting leaves of both `cot_out_mean` and `cot_out_std` pytrees to
+        zeros can disable the corresponding output cotangents.
+    """
     fun_orig = fun.__wrapped__  # original function without custom vjp
+
     if args or kwargs:
         fun = partial(fun, *args, **kwargs)
         fun_orig = partial(fun_orig, *args, **kwargs)
 
     primals_out, vjpfun = vjp(fun, *primals)
-    cot_out = tree_map(partial(randn_like, key), primals_out)
-    cot = vjpfun(cot_out)
-
     primals_out_orig, vjpfun_orig = vjp(fun_orig, *primals)
-    cot_orig = vjpfun_orig(cot_out)
-
     jtu.check_close(primals_out, primals_out_orig, atol=atol, rtol=rtol)
+
+    cot_out = tree_randn_float0_like(primals_out, cot_out_mean, cot_out_std)
+
+    cot = vjpfun(cot_out)
+    cot_orig = vjpfun_orig(cot_out)
+    cot = tree_zero_leaves(cot, cot_skip)
+    cot_orig = tree_zero_leaves(cot_orig, cot_skip)
     jtu.check_close(cot, cot_orig, atol=atol, rtol=rtol)
