72ed0bb9225c8b0bc978e8758a624b1c78ef8934
Yin Li <eelregit@gmail.com>
2022-03-09 19:49:04 -0500

    Add Cosmology and util

---
+++ pmwd/cosmology.py
@@ -0,0 +1,202 @@
+from dataclasses import field
+from functools import partial
+from pprint import pformat
+from typing import Optional
+
+from jax import jit, value_and_grad, vmap
+import jax.numpy as jnp
+
+from pmwd.conf import Configuration
+from pmwd.dataclasses import pytree_dataclass
+
+
+@partial(pytree_dataclass, aux_fields="conf", frozen=True)
+class Cosmology:
+    """Cosmological and configuration parameters, "immutable" as a frozen dataclass.
+    Cosmological parameters are traced by JAX while configuration parameters are not.
+
+    Parameters
+    ----------
+    conf : Configuration
+        Configuration parameters, not traced by JAX.
+    A_s_1e9 : float
+        Primordial scalar power spectrum amplitude, multiplied by 1e9.
+    n_s : float
+        Primordial scalar power spectrum spectral index.
+    Omega_m : float
+        Total matter density parameter today.
+    Omega_b : float
+        Baryonic matter density parameter today.
+    Omega_k : float
+        Spatial curvature density parameter today.
+    w_0 : float
+        First order term of dark energy equation.
+    w_a : float
+        Second order term of dark energy equation of state.
+    h : float
+        Hubble constant in unit of 100 [km/s/Mpc].
+
+    """
+
+    conf: Configuration
+
+    A_s_1e9: float
+    n_s: float
+    Omega_m: float
+    Omega_b: float
+    Omega_k: float
+    w_0: float
+    w_a: float
+    h: float
+
+    transfer: Optional[jnp.ndarray] = field(default=None, repr=False, compare=False)
+
+    growth: Optional[jnp.ndarray] = field(default=None, repr=False, compare=False)
+
+    def __str__(self):
+        return pformat(self, indent=4, width=1)  # for python >= 3.10
+
+    @property
+    def k_pivot(self):
+        """Primordial scalar power spectrum pivot scale in [1/L].
+
+        Notes
+        -----
+        Pivot scale is defined h-less unit, so needs h to convert its unit to [1/L].
+        """
+        return self.conf.k_pivot_Mpc / (self.h * self.conf.Mpc_SI) * self.conf.L
+
+    @property
+    def Omega_c(self):
+        """Cold dark matter density parameter today."""
+        return self.Omega_m - self.Omega_b
+
+    @property
+    def Omega_de(self):
+        """Dark energy density parameter today."""
+        return 1. - (self.Omega_m + self.Omega_k)
+
+
+SimpleLCDM = partial(
+    Cosmology,
+    A_s_1e9=2.0,
+    n_s=0.96,
+    Omega_m=0.3,
+    Omega_b=0.05,
+    Omega_k=0.0,
+    w_0=-1.0,
+    w_a=0.0,
+    h=0.7,
+)
+SimpleLCDM.__doc__ = "Simple ΛCDM cosmology, for convenience and subject to change."
+
+Planck18 = partial(
+    Cosmology,
+    A_s_1e9=2.105,
+    n_s=0.9665,
+    Omega_m=0.3111,
+    Omega_b=0.04897,
+    Omega_k=0.0,
+    w_0=-1.0,
+    w_a=0.0,
+    h=0.6766,
+)
+Planck18.__doc__ = "Planck 2018 cosmology, arXiv:1807.06209 Table 2 last column."
+
+
+def _E2(a, cosmo):
+    de_a = (a**(-3.0 * (1.0 + cosmo.w_0 + cosmo.w_a))
+           * jnp.exp(-3.0 * cosmo.w_a * (1.0 - a)))
+    return cosmo.Omega_m * a**-3 + cosmo.Omega_k * a**-2 + cosmo.Omega_de * de_a
+
+_E2_vmap = vmap(_E2, in_axes=(0, None))
+_E2_valgrad_vmap = vmap(value_and_grad(_E2), in_axes=(0, None))
+
+
+def E2(a, cosmo):
+    r"""Squared Hubble parameter time scaling factors, :math:`E^2`, at given scale
+    factors.
+
+    Parameters
+    ----------
+    a : array_like
+        Scale factors.
+    cosmo : Cosmology
+
+    Returns
+    -------
+    E2 : jax.numpy.ndarray
+        Squared Hubble parameter time scaling factors.
+
+    Notes
+    -----
+
+    The squared Hubble parameter
+
+    .. math::
+
+        H^2(a) = H_0^2 E^2(a),
+
+    has time scaling
+
+    .. math::
+
+        E^2(a) = \Omega_\mathrm{m} a^{-3} + \Omega_\mathrm{k} a^{-2}
+                 + \Omega_\mathrm{de} a^{-3 (1 + w_0 + w_a)} e^{-3 w_a (1 - a)}.
+
+    """
+    a = jnp.asarray(a)
+    shape = a.shape
+    a = jnp.ravel(a)
+    E2 = _E2_vmap(a, cosmo)
+    return E2.reshape(shape)
+
+
+def H_deriv(a, cosmo):
+    r"""Hubble parameter derivatives, :math:`\mathrm{d}\ln H / \mathrm{d}\ln a`, at
+    given scale factors.
+
+    Parameters
+    ----------
+    a : array_like
+        Scale factors.
+    cosmo : Cosmology
+
+    Returns
+    -------
+    dlnH_dlna : jax.numpy.ndarray
+        Hubble parameter derivatives.
+
+    """
+    a = jnp.asarray(a)
+    shape = a.shape
+    a = jnp.ravel(a)
+    E2, dE2_da = _E2_valgrad_vmap(a, cosmo)
+    dlnH_dlna = 0.5 * a * dE2_da / E2
+    return dlnH_dlna.reshape(shape)
+
+
+def Omega_m_a(a, cosmo):
+    r"""Matter density parameters, :math:`\Omega_\mathrm{m}(a)`, at given scale factors.
+
+    Parameters
+    ----------
+    a : array_like
+        Scale factors.
+    cosmo : Cosmology
+
+    Returns
+    -------
+    Omega : jax.numpy.ndarray
+        Matter density parameters.
+
+    Notes
+    -----
+
+    .. math::
+
+        \Omega_\mathrm{m}(a) = \frac{\Omega_\mathrm{m} a^{-3}}{E^2(a)}
+
+    """
+    a = jnp.asarray(a)
+    return cosmo.Omega_m / (a**3 * E2(a, cosmo))
