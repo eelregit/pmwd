9d17f97149a361b35d0564c315fb58e2a4bcd2da
Yin Li <eelregit@gmail.com>
2022-04-15 11:30:35 -0400

    Add __init__ imports

--- pmwd/__init__.py
+++ pmwd/__init__.py
@@ -1,13 +1,18 @@
-"""pmwd: particle mesh with derivatives
-
-Differentiable Cosmological Particle-Mesh Forward Model
-"""
+"""pmwd: particle mesh with derivatives"""
 
 
-from importlib.metadata import version, PackageNotFoundError
-
-
+from pmwd.tree_util import pytree_dataclass
+from pmwd.conf import Configuration
+from pmwd.cosmology import Cosmology, DMO, SimpleLCDM, Planck18, E2, H_deriv, Omega_m_a
+from pmwd.boltzmann import (transfer_integ, transfer_fit, transfer,
+                            growth_integ, growth, boltzmann, linear_power)
+from pmwd.particles import Particles, gen_ptcl, ptcl_pos, ptcl_rpos, ptcl_rsd, ptcl_los
+from pmwd.scatter import scatter
+from pmwd.gather import gather
+from pmwd.gravity import rfftnfreq, laplace, neg_grad, gravity
+from pmwd.lpt import white_noise, linear_modes, lpt
+from pmwd.nbody import nbody
 try:
-    __version__ = version('pmwd')
-except PackageNotFoundError:
+    from pmwd.version import __version__
+except ImportError:
     pass  # not installed
