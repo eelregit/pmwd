cbf0b4da5b8b68f7cfef00c3d3a155931f16941b
Yin Li <eelregit@gmail.com>
2022-06-12 15:54:15 -0400

    FIXUP __init__: refactor and add enmesh

--- pmwd/__init__.py
+++ pmwd/__init__.py
@@ -6,12 +6,14 @@ from pmwd.configuration import Configuration
 from pmwd.cosmology import Cosmology, SimpleLCDM, Planck18, E2, H_deriv, Omega_m_a
 from pmwd.boltzmann import (transfer_integ, transfer_fit, transfer,
                             growth_integ, growth, boltzmann, linear_power)
-from pmwd.particles import Particles, ptcl_pos, ptcl_rpos, ptcl_rsd, ptcl_los
+from pmwd.particles import (Particles, ptcl_enmesh,
+                            ptcl_pos, ptcl_rpos, ptcl_rsd, ptcl_los)
 from pmwd.scatter import scatter
 from pmwd.gather import gather
-from pmwd.gravity import rfftnfreq, laplace, neg_grad, gravity
+from pmwd.gravity import laplace, neg_grad, gravity
 from pmwd.lpt import white_noise, linear_modes, lpt
 from pmwd.nbody import nbody
+from pmwd.pm_util import enmesh, rfftnfreq
 try:
     from pmwd.version import __version__
 except ImportError:
