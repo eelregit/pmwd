7b653d7be8c8496d75d0ecf9772ec0f7d5fc79f8
Yin Li <eelregit@gmail.com>
2022-06-12 16:06:23 -0400

    FIXUP scatter gather: refactor and generalize

--- pmwd/gather.py
+++ pmwd/gather.py
@@ -1,29 +1,50 @@
-from functools import partial
-
 import jax.numpy as jnp
 from jax import custom_vjp
 from jax.lax import scan
 
-from pmwd.scatter import _chunk_split, _chunk_cat
+from pmwd.pm_util import _chunk_split, _chunk_cat, enmesh
 
 
-def gather(ptcl, mesh, val, cell_size, chunk_size=None):
-    """Gather particle values from mesh in n-D with trilinear scheme."""
-    return _gather(ptcl.pmid, ptcl.disp, mesh, val, cell_size, chunk_size)
+def gather(ptcl, conf, mesh, val=0, offset=0, cell_size=None):
+    """Gather particle values from mesh multilinearly in n-D.
+
+    Parameters
+    ----------
+    ptcl : Particles
+    conf : Configuration
+    mesh : array_like
+        Input mesh.
+    val : array_like, optional
+        Input values, can be 0D.
+    offset : array_like, optional
+        Offset of mesh to particle grid. If 0D, the value is used in each dimension.
+    cell_size : float, optional
+        Mesh cell size in [L]. Default is ``conf.cell_size``.
+
+    Returns
+    -------
+    val : jax.numpy.ndarray
+        Output values.
+
+    """
+    return _gather(ptcl.pmid, ptcl.disp, conf, mesh, val, offset, cell_size)
 
 
-@partial(custom_vjp, nondiff_argnums=(5,))
-def _gather(pmid, disp, mesh, val, cell_size, chunk_size):
-    ptcl_num = pmid.shape[0]
+@custom_vjp
+def _gather(pmid, disp, conf, mesh, val, offset, cell_size):
+    ptcl_num, spatial_ndim = pmid.shape
 
-    val = jnp.asarray(val, dtype=disp.dtype)
+    mesh = jnp.asarray(mesh, dtype=conf.float_dtype)
 
-    if val.ndim == 0:
-        val = jnp.full(ptcl_num, val)
+    val = jnp.asarray(val, dtype=conf.float_dtype)
 
-    remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val)
+    if mesh.shape[spatial_ndim:] != val.shape[1:]:
+        raise ValueError('channel shape mismatch: '
+                         f'{mesh.shape[spatial_ndim:]} != {val.shape[1:]}')
 
-    carry = mesh, cell_size
+    remainder, chunks = _chunk_split(ptcl_num, conf.chunk_size, pmid, disp, val)
+
+    carry = conf, mesh, offset, cell_size
     val_0 = None
     if remainder is not None:
         val_0 = _gather_chunk(carry, remainder)[1]
@@ -35,39 +56,23 @@ def _gather(pmid, disp, mesh, val, cell_size, chunk_size):
 
 
 def _gather_chunk(carry, chunk):
-    mesh, cell_size = carry
+    conf, mesh, offset, cell_size = carry
     pmid, disp, val = chunk
 
-    ptcl_num, spatial_ndim = pmid.shape
+    spatial_ndim = pmid.shape[1]
 
     spatial_shape = mesh.shape[:spatial_ndim]
     chan_ndim = mesh.ndim - spatial_ndim
     chan_axis = tuple(range(-chan_ndim, 0))
 
-    disp /= cell_size
-
-    # insert neighbor axis
-    pmid = pmid[:, jnp.newaxis]
-    disp = disp[:, jnp.newaxis]
-
-    # trilinear
-    neighbors = (jnp.arange(2 ** spatial_ndim, dtype=pmid.dtype)[:, jnp.newaxis]
-                 >> jnp.arange(spatial_ndim, dtype=pmid.dtype)
-                ) & 1
-    tgt = jnp.floor(disp).astype(pmid.dtype)
-    tgt += neighbors
-    frac = 1 - jnp.abs(disp - tgt)
-    frac = frac.prod(axis=-1)
-    frac = jnp.expand_dims(frac, chan_axis)
-    tgt += pmid
-
-    # periodic boundaries
-    # TODO no wrapping for parallelization
-    tgt %= jnp.array(spatial_shape, dtype=pmid.dtype)
+    # multilinear mesh indices and fractions
+    ind, frac = enmesh(pmid, disp, conf.cell_size, conf.mesh_shape,
+                       offset, cell_size, spatial_shape, False)
 
     # gather
-    tgt = tuple(tgt[..., i] for i in range(spatial_ndim))
-    val += (mesh[tgt] * frac).sum(axis=1)
+    ind = tuple(ind[..., i] for i in range(spatial_ndim))
+    frac = jnp.expand_dims(frac, chan_axis)
+    val += (mesh.at[ind].get(mode='drop', fill_value=0) * frac).sum(axis=1)  # TODO indices_are_sorted=True
 
     return carry, val
 
@@ -80,78 +85,60 @@ def _gather_chunk_adj(carry, chunk):
     Scatter val_cot to mesh_cot.
 
     """
-    mesh, mesh_cot, cell_size = carry
+    conf, mesh, mesh_cot, offset, cell_size = carry
     pmid, disp, val_cot = chunk
 
-    ptcl_num, spatial_ndim = pmid.shape
+    spatial_ndim = pmid.shape[1]
 
     spatial_shape = mesh.shape[:spatial_ndim]
     chan_ndim = mesh.ndim - spatial_ndim
     chan_axis = tuple(range(-chan_ndim, 0))
 
-    disp /= cell_size
+    # multilinear mesh indices and fractions
+    ind, frac, frac_grad = enmesh(pmid, disp, conf.cell_size, conf.mesh_shape,
+                                  offset, cell_size, spatial_shape, True)
 
-    # insert neighbor axis
-    pmid = pmid[:, jnp.newaxis]
-    disp = disp[:, jnp.newaxis]
-    val_cot = val_cot[:, jnp.newaxis]
-
-    # trilinear
-    neighbors = (jnp.arange(2 ** spatial_ndim, dtype=pmid.dtype)[:, jnp.newaxis]
-                 >> jnp.arange(spatial_ndim, dtype=pmid.dtype)
-                ) & 1
-    tgt = jnp.floor(disp).astype(pmid.dtype)
-    tgt += neighbors
-    frac = 1 - jnp.abs(disp - tgt)
-    sign = jnp.sign(tgt - disp)
-    frac_grad = []
-    for i in range(spatial_ndim):
-        not_i = tuple(range(0, i)) + tuple(range(i + 1, spatial_ndim))
-        frac_grad.append(sign[..., i] * frac[..., not_i].prod(axis=-1))
-    frac_grad = jnp.stack(frac_grad, axis=-1)
-    frac = frac.prod(axis=-1)
-    frac = jnp.expand_dims(frac, chan_axis)
-    tgt += pmid
-
-    # periodic boundaries
-    # TODO no wrapping for parallelization
-    tgt %= jnp.array(spatial_shape, dtype=pmid.dtype)
+    if val_cot.ndim != 0:
+        val_cot = val_cot[:, jnp.newaxis]  # insert neighbor axis
 
     # gather disp_cot from val_cot and mesh, and scatter val_cot to mesh_cot
-    tgt = tuple(tgt[..., i] for i in range(spatial_ndim))
-    val = mesh[tgt]
+    ind = tuple(ind[..., i] for i in range(spatial_ndim))
+    val = mesh.at[ind].get(mode='drop', fill_value=0)  # TODO indices_are_sorted=True
 
     disp_cot = (val_cot * val).sum(axis=chan_axis)
     disp_cot = (disp_cot[..., jnp.newaxis] * frac_grad).sum(axis=1)
     disp_cot /= cell_size
 
-    mesh_cot = mesh_cot.at[tgt].add(val_cot * frac)
+    frac = jnp.expand_dims(frac, chan_axis)
+    mesh_cot = mesh_cot.at[ind].add(val_cot * frac)  # TODO indices_are_sorted=True
 
-    carry = mesh, mesh_cot, cell_size
+    carry = conf, mesh, mesh_cot, offset, cell_size
     return carry, disp_cot
 
 
-def _gather_fwd(pmid, disp, mesh, val, cell_size, chunk_size):
-    val = _gather(pmid, disp, mesh, val, cell_size, chunk_size)
-    return val, (pmid, disp, mesh, cell_size)
+def _gather_fwd(pmid, disp, conf, mesh, val, offset, cell_size):
+    val = _gather(pmid, disp, conf, mesh, val, offset, cell_size)
+    return val, (pmid, disp, conf, mesh, offset, cell_size)
 
-def _gather_bwd(chunk_size, res, val_cot):
-    pmid, disp, mesh, cell_size = res
+def _gather_bwd(res, val_cot):
+    pmid, disp, conf, mesh, offset, cell_size = res
 
-    ptcl_num = pmid.shape[0]
+    ptcl_num = len(pmid)
 
-    remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val_cot)
-
+    mesh = jnp.asarray(mesh, dtype=conf.float_dtype)
     mesh_cot = jnp.zeros_like(mesh)
-    carry = mesh, mesh_cot, cell_size
+
+    remainder, chunks = _chunk_split(ptcl_num, conf.chunk_size, pmid, disp, val_cot)
+
+    carry = conf, mesh, mesh_cot, offset, cell_size
     disp_cot_0 = None
     if remainder is not None:
         carry, disp_cot_0 = _gather_chunk_adj(carry, remainder)
     carry, disp_cot = scan(_gather_chunk_adj, carry, chunks)
-    mesh_cot = carry[1]
+    mesh_cot = carry[2]
 
     disp_cot = _chunk_cat(disp_cot_0, disp_cot)
 
-    return None, disp_cot, mesh_cot, val_cot, None
+    return None, disp_cot, None, mesh_cot, val_cot, None, None
 
 _gather.defvjp(_gather_fwd, _gather_bwd)
--- pmwd/scatter.py
+++ pmwd/scatter.py
@@ -1,98 +1,85 @@
-from functools import partial
-
 import jax.numpy as jnp
 from jax import custom_vjp
 from jax.lax import scan
 
-
-def _chunk_split(ptcl_num, chunk_size, *arrays):
-    """Split and reshape particle arrays into chunks and a remainder."""
-    chunk_size = ptcl_num if chunk_size is None else min(chunk_size, ptcl_num)
-    remainder_size = ptcl_num % chunk_size
-    chunk_num = ptcl_num // chunk_size
-
-    remainder = None
-    chunks = arrays
-    if remainder_size:
-        remainder = [x[:remainder_size] for x in arrays]
-        chunks = [x[remainder_size:] for x in arrays]
-
-    chunks = [x.reshape(chunk_num, chunk_size, *x.shape[1:]) for x in chunks]
-
-    return remainder, chunks
-
-def _chunk_cat(remainder_array, chunks_array):
-    """Reshape and concatenate a remainder and a chunked particle arrays."""
-    array = chunks_array.reshape(-1, *chunks_array.shape[2:])
-
-    if remainder_array is not None:
-        array = jnp.concatenate((remainder_array, array), axis=0)
-
-    return array
+from pmwd.pm_util import _chunk_split, _chunk_cat, enmesh
 
 
-def scatter(ptcl, mesh, val, cell_size, chunk_size=None):
-    """Scatter particle values to mesh in n-D with trilinear scheme."""
-    return _scatter(ptcl.pmid, ptcl.disp, mesh, val, cell_size, chunk_size)
+def scatter(ptcl, conf, mesh=None, val=None, offset=0, cell_size=None):
+    """Scatter particle values to mesh multilinearly in n-D.
+
+    Parameters
+    ----------
+    ptcl : Particles
+    conf : Configuration
+    mesh : array_like, optional
+        Input mesh. Default is a ``zeros`` array of ``conf.mesh_shape + val.shape[1:]``.
+    val : array_like, optional
+        Input values, can be 0D. Default is ``conf.mesh_size / conf.ptcl_num``.
+    offset : array_like, optional
+        Offset of mesh to particle grid. If 0D, the value is used in each dimension.
+    cell_size : float, optional
+        Mesh cell size in [L]. Default is ``conf.cell_size``.
+
+    Returns
+    -------
+    mesh : jax.numpy.ndarray
+        Output mesh.
+
+    """
+    return _scatter(ptcl.pmid, ptcl.disp, conf, mesh, val, offset, cell_size)
 
 
-@partial(custom_vjp, nondiff_argnums=(5,))
-def _scatter(pmid, disp, mesh, val, cell_size, chunk_size):
-    ptcl_num = pmid.shape[0]
+@custom_vjp
+def _scatter(pmid, disp, conf, mesh, val, offset, cell_size):
+    ptcl_num, spatial_ndim = pmid.shape
 
-    val = jnp.asarray(val, dtype=disp.dtype)
+    if val is None:
+        val = conf.mesh_size / conf.ptcl_num
+    val = jnp.asarray(val, dtype=conf.float_dtype)
 
-    if val.ndim == 0:
-        val = jnp.full(ptcl_num, val)
+    if mesh is None:
+        mesh = jnp.zeros(conf.mesh_shape + val.shape[1:], dtype=conf.float_dtype)
+    mesh = jnp.asarray(mesh, dtype=conf.float_dtype)
 
-    remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val)
+    if mesh.shape[spatial_ndim:] != val.shape[1:]:
+        raise ValueError('channel shape mismatch: '
+                         f'{mesh.shape[spatial_ndim:]} != {val.shape[1:]}')
 
-    carry = mesh, cell_size
+    remainder, chunks = _chunk_split(ptcl_num, conf.chunk_size, pmid, disp, val)
+
+    carry = conf, mesh, offset, cell_size
     if remainder is not None:
         carry = _scatter_chunk(carry, remainder)[0]
     carry = scan(_scatter_chunk, carry, chunks)[0]
-    mesh = carry[0]
+    mesh = carry[1]
 
     return mesh
 
 
 def _scatter_chunk(carry, chunk):
-    mesh, cell_size = carry
+    conf, mesh, offset, cell_size = carry
     pmid, disp, val = chunk
 
-    ptcl_num, spatial_ndim = pmid.shape
+    spatial_ndim = pmid.shape[1]
 
     spatial_shape = mesh.shape[:spatial_ndim]
     chan_ndim = mesh.ndim - spatial_ndim
     chan_axis = tuple(range(-chan_ndim, 0))
 
-    disp /= cell_size
+    # multilinear mesh indices and fractions
+    ind, frac = enmesh(pmid, disp, conf.cell_size, conf.mesh_shape,
+                       offset, cell_size, spatial_shape, False)
 
-    # insert neighbor axis
-    pmid = pmid[:, jnp.newaxis]
-    disp = disp[:, jnp.newaxis]
-    val = val[:, jnp.newaxis]
-
-    # trilinear
-    neighbors = (jnp.arange(2**spatial_ndim, dtype=pmid.dtype)[:, jnp.newaxis]
-                 >> jnp.arange(spatial_ndim, dtype=pmid.dtype)
-                ) & 1
-    tgt = jnp.floor(disp).astype(pmid.dtype)
-    tgt += neighbors
-    frac = 1 - jnp.abs(disp - tgt)
-    frac = frac.prod(axis=-1)
-    frac = jnp.expand_dims(frac, chan_axis)
-    tgt += pmid
-
-    # periodic boundaries
-    # TODO no wrapping for parallelization
-    tgt %= jnp.array(spatial_shape, dtype=pmid.dtype)
+    if val.ndim != 0:
+        val = val[:, jnp.newaxis]  # insert neighbor axis
 
     # scatter
-    tgt = tuple(tgt[..., i] for i in range(spatial_ndim))
-    mesh = mesh.at[tgt].add(val * frac)
+    ind = tuple(ind[..., i] for i in range(spatial_ndim))
+    frac = jnp.expand_dims(frac, chan_axis)
+    mesh = mesh.at[ind].add(val * frac)  # TODO indices_are_sorted=True
 
-    carry = mesh, cell_size
+    carry = conf, mesh, offset, cell_size
     return carry, None
 
 
@@ -104,73 +91,52 @@ def _scatter_chunk_adj(carry, chunk):
     Gather val_cot from mesh_cot.
 
     """
-    mesh_cot, cell_size = carry
+    conf, mesh_cot, offset, cell_size = carry
     pmid, disp, val = chunk
 
-    ptcl_num, spatial_ndim = pmid.shape
+    spatial_ndim = pmid.shape[1]
 
     spatial_shape = mesh_cot.shape[:spatial_ndim]
     chan_ndim = mesh_cot.ndim - spatial_ndim
     chan_axis = tuple(range(-chan_ndim, 0))
 
-    disp /= cell_size
+    # multilinear mesh indices and fractions
+    ind, frac, frac_grad = enmesh(pmid, disp, conf.cell_size, conf.mesh_shape,
+                                  offset, cell_size, spatial_shape, True)
 
-    # insert neighbor axis
-    pmid = pmid[:, jnp.newaxis]
-    disp = disp[:, jnp.newaxis]
-    val = val[:, jnp.newaxis]
-
-    # trilinear
-    neighbors = (jnp.arange(2**spatial_ndim, dtype=pmid.dtype)[:, jnp.newaxis]
-                 >> jnp.arange(spatial_ndim, dtype=pmid.dtype)
-                ) & 1
-    tgt = jnp.floor(disp).astype(pmid.dtype)
-    tgt += neighbors
-    frac = 1 - jnp.abs(disp - tgt)
-    sign = jnp.sign(tgt - disp)
-    frac_grad = []
-    for i in range(spatial_ndim):
-        not_i = tuple(range(0, i)) + tuple(range(i + 1, spatial_ndim))
-        frac_grad.append(sign[..., i] * frac[..., not_i].prod(axis=-1))
-    frac_grad = jnp.stack(frac_grad, axis=-1)
-    frac = frac.prod(axis=-1)
-    frac = jnp.expand_dims(frac, chan_axis)
-    tgt += pmid
-
-    # periodic boundaries
-    # TODO no wrapping for parallelization
-    tgt %= jnp.array(spatial_shape, dtype=pmid.dtype)
+    if val.ndim != 0:
+        val = val[:, jnp.newaxis]  # insert neighbor axis
 
     # gather disp_cot from mesh_cot and val, and gather val_cot from mesh_cot
-    tgt = tuple(tgt[..., i] for i in range(spatial_ndim))
-    val_cot = mesh_cot[tgt]
+    ind = tuple(ind[..., i] for i in range(spatial_ndim))
+    val_cot = mesh_cot.at[ind].get(mode='drop', fill_value=0)  # TODO indices_are_sorted=True
 
     disp_cot = (val_cot * val).sum(axis=chan_axis)
     disp_cot = (disp_cot[..., jnp.newaxis] * frac_grad).sum(axis=1)
     disp_cot /= cell_size
 
+    frac = jnp.expand_dims(frac, chan_axis)
     val_cot = (val_cot * frac).sum(axis=1)
 
     return carry, (disp_cot, val_cot)
 
 
-def _scatter_fwd(pmid, disp, mesh, val, cell_size, chunk_size):
-    mesh = _scatter(pmid, disp, mesh, val, cell_size, chunk_size)
-    return mesh, (pmid, disp, val, cell_size)
+def _scatter_fwd(pmid, disp, conf, mesh, val, offset, cell_size):
+    mesh = _scatter(pmid, disp, conf, mesh, val, offset, cell_size)
+    return mesh, (pmid, disp, conf, val, offset, cell_size)
 
-def _scatter_bwd(chunk_size, res, mesh_cot):
-    pmid, disp, val, cell_size = res
+def _scatter_bwd(res, mesh_cot):
+    pmid, disp, conf, val, offset, cell_size = res
 
-    ptcl_num = pmid.shape[0]
+    ptcl_num = len(pmid)
 
-    val = jnp.asarray(val, dtype=disp.dtype)
+    if val is None:
+        val = conf.mesh_size / conf.ptcl_num
+    val = jnp.asarray(val, dtype=conf.float_dtype)
 
-    if val.ndim == 0:
-        val = jnp.full(ptcl_num, val)
+    remainder, chunks = _chunk_split(ptcl_num, conf.chunk_size, pmid, disp, val)
 
-    remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val)
-
-    carry = mesh_cot, cell_size
+    carry = conf, mesh_cot, offset, cell_size
     disp_cot_0, val_cot_0 = None, None
     if remainder is not None:
         disp_cot_0, val_cot_0 = _scatter_chunk_adj(carry, remainder)[1]
@@ -179,6 +145,6 @@ def _scatter_bwd(chunk_size, res, mesh_cot):
     disp_cot = _chunk_cat(disp_cot_0, disp_cot)
     val_cot = _chunk_cat(val_cot_0, val_cot)
 
-    return None, disp_cot, mesh_cot, val_cot, None
+    return None, disp_cot, None, mesh_cot, val_cot, None, None
 
 _scatter.defvjp(_scatter_fwd, _scatter_bwd)
