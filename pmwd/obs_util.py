16aff39e9da8ce68d3a1bfe74f0c287fd10cc5c3
Yucheng Zhang <yczhang.nyu@gmail.com>
2022-11-28 16:23:59 +0800

    FIXUP refine interp_ptcl function

--- pmwd/obs_util.py
+++ pmwd/obs_util.py
@@ -10,24 +10,23 @@ def interp_ptcl(ptcl0, ptcl1, a0, a1, a, cosmo, conf):
     """
     Da = a1 - a0
     t = (a - a0) / Da
-    # convert to ddisp/da
-    vel0 = ptcl0.vel / (a0**3 * jnp.sqrt(E2(a0, cosmo)))
-    vel1 = ptcl1.vel / (a1**3 * jnp.sqrt(E2(a1, cosmo)))
+    a3E0 = a0**3 * jnp.sqrt(E2(a0, cosmo))
+    a3E1 = a1**3 * jnp.sqrt(E2(a1, cosmo))
     # displacement
     h00 = 2 * t**3 - 3 * t**2 + 1
     h10 = t**3 - 2 * t**2 + t
     h01 = - 2 * t**3 + 3 * t**2
     h11 = t**3 - t**2
-    disp = (h00 * ptcl0.disp + h10 * Da * vel0 +
-            h01 * ptcl1.disp + h11 * Da * vel1)
+    disp = (h00 * ptcl0.disp + h10 * Da / a3E0 * ptcl0.vel +
+            h01 * ptcl1.disp + h11 * Da / a3E1 * ptcl1.vel)
     # velocity
     # derivatives of the Hermite basis functions
     h00 = 6 * t**2 - 6 * t
     h10 = 3 * t**2 - 4 * t + 1
     h01 = - 6 * t**2 + 6 * t
     h11 = 3 * t**2 - 2 * t
-    vel = (h00 * ptcl0.disp / Da + h10 * vel0 +
-           h01 * ptcl1.disp / Da + h11 * vel1)
+    vel = (h00 / Da * ptcl0.disp + h10 / a3E0 * ptcl0.vel +
+           h01 / Da * ptcl1.disp + h11 / a3E1 * ptcl1.vel)
     vel *= a**3 * jnp.sqrt(E2(a, cosmo))
 
     ptcl = Particles(conf, ptcl0.pmid, disp, vel=vel)
