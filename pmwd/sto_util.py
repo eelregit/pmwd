f0c14a8c5d3f93f692cab5699f2939813b2e827b
Yucheng Zhang <yczhang.nyu@gmail.com>
2022-11-30 22:51:17 +0800

    Add option to init all mlp params with zero

--- pmwd/sto_util.py
+++ pmwd/sto_util.py
@@ -1,10 +1,12 @@
 import jax
 import jax.numpy as jnp
 from jax import random
+from jax.tree_util import tree_map
 import flax.linen as nn
 from flax.core.frozen_dict import unfreeze, freeze
 from typing import Sequence
 import math
+
 from pmwd.pm_util import rfftnfreq
 
 
@@ -19,18 +21,25 @@ class MLP(nn.Module):
         for i, lyr in enumerate(self.layers):
             x = lyr(x)
             if i != len(self.layers)-1:
-                x = nn.relu(x)
+                x = nn.softplus(x)
         return x
 
 
-def init_mlp_params(n_input, nodes, zero_last_weights=False):
+def init_mlp_params(n_input, nodes, zero_params=None):
     """Initialized MLP parameters."""
     nets = [MLP(features=n) for n in nodes]
     xs = [jnp.ones(n) for n in n_input]  # dummy inputs
     keys = random.split(random.PRNGKey(0), len(n_input))
+
+    # by default in flax.linen.Dense, kernel: lecun_norm, bias: 0
     params = [nn.init(key, x) for nn, key, x in zip(nets, keys, xs)]
 
-    if zero_last_weights:  # set the weights of the last layer to zero
+    # set all params to zero
+    if zero_params == 'all':
+        params = tree_map(lambda x: jnp.zeros_like(x), params)
+
+    # set the params of the last layer to zero, bias = zero by default
+    if zero_params == 'last':  
         for i, p in enumerate(params):
             p = unfreeze(p)
             p['params'][f'layers_{len(nodes)-1}']['kernel'] = (
