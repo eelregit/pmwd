b5530e0c95bd7f35c63872bd1914e5ab17ace4c1
Yucheng Zhang <yczhang.nyu@gmail.com>
2022-12-19 19:08:17 +0800

    Add interp with prev and next snaps separately

--- pmwd/obs_util.py
+++ pmwd/obs_util.py
@@ -4,7 +4,43 @@ from pmwd.particles import Particles
 from pmwd.cosmology import E2
 
 
-def interp_ptcl(ptcl0, ptcl1, a0, a1, a, cosmo, conf):
+def itp_prev(ptcl0, a0, a1, a, cosmo):
+    Da = a1 - a0
+    t = (a - a0) / Da
+    a3E0 = a0**3 * jnp.sqrt(E2(a0, cosmo))
+    # displacement
+    h00 = 2 * t**3 - 3 * t**2 + 1
+    h10 = t**3 - 2 * t**2 + t
+    disp = h00 * ptcl0.disp + h10 * Da / a3E0 * ptcl0.vel
+    # velocity
+    # derivatives of the Hermite basis functions
+    h00 = 6 * t**2 - 6 * t
+    h10 = 3 * t**2 - 4 * t + 1
+    vel = h00 / Da * ptcl0.disp + h10 / a3E0 * ptcl0.vel
+    vel *= a**3 * jnp.sqrt(E2(a, cosmo))
+
+    return disp, vel
+
+
+def itp_next(ptcl1, a0, a1, a, cosmo):
+    Da = a1 - a0
+    t = (a - a0) / Da
+    a3E1 = a1**3 * jnp.sqrt(E2(a1, cosmo))
+    # displacement
+    h01 = - 2 * t**3 + 3 * t**2
+    h11 = t**3 - t**2
+    disp = h01 * ptcl1.disp + h11 * Da / a3E1 * ptcl1.vel
+    # velocity
+    # derivatives of the Hermite basis functions
+    h01 = - 6 * t**2 + 6 * t
+    h11 = 3 * t**2 - 2 * t
+    vel = h01 / Da * ptcl1.disp + h11 / a3E1 * ptcl1.vel
+    vel *= a**3 * jnp.sqrt(E2(a, cosmo))
+
+    return disp, vel
+
+
+def interptcl(ptcl0, ptcl1, a0, a1, a, cosmo):
     """Given two ptcl snapshots, get the interpolated one
        at a given time using cubic Hermite interpolation.
     """
@@ -29,5 +65,5 @@ def interp_ptcl(ptcl0, ptcl1, a0, a1, a, cosmo, conf):
            h01 / Da * ptcl1.disp + h11 / a3E1 * ptcl1.vel)
     vel *= a**3 * jnp.sqrt(E2(a, cosmo))
 
-    ptcl = Particles(conf, ptcl0.pmid, disp, vel=vel)
+    ptcl = Particles(ptcl0.conf, ptcl0.pmid, disp, vel=vel)
     return ptcl
