ebba255eb6f4deba92668f89d36339e8d099da1b
Yin Li <eelregit@gmail.com>
2023-02-12 13:48:27 +0800

    FIXUP setup_g4sims: misc

--- docs/examples/sto/g4gen/setup_g4sims.py
+++ docs/examples/sto/g4gen/setup_g4sims.py
@@ -1,15 +1,15 @@
 """A script to setup directories and files etc for Gadget-4 simulations."""
 import os
+import sys
+
 import numpy as np
+
 from pmwd.train_util import gen_ic
 from pmwd.io_util import write_gadget_hdf5
 
 
-def gen_g4files(sim_dir, i,
-                fn_sobol='sobol.txt',
-                tpl_config='Config.sh',
-                tpl_param='param.txt',
-                tpl_job='job.sh'):
+def gen_g4files(sim_dir, i, fn_sobol='sobol.txt',
+                tpl_config='Config.sh', tpl_param='param.txt', tpl_job='job.sh'):
     """
     Generate input files with Sobol i parameters for Gadget4 dark matter only
     simulations.
@@ -37,26 +37,27 @@ def gen_g4files(sim_dir, i,
     with (open(tpl_param, 'r') as f,
           open(os.path.join(sim_dir, 'param.txt'), 'w') as fo):
         param = f.read()
-        param = param.format(a_start=conf.a_start,
-                             a_stop=conf.a_stop,
-                             box_size=conf.box_size[0],
-                             M_g=conf.M*1e3*cosmo.h,  # TODO double check the units
-                             L_cm=conf.L*1e2*cosmo.h,
-                             V_cm_s=conf.V*1e5,
-                             H_0=100.0,
-                             G=0,
-                             Omega_m=cosmo.Omega_m,
-                             Omega_de=cosmo.Omega_de,
-                             Omega_b=cosmo.Omega_b,
-                             h=cosmo.h,
-                             soften_cmv=sobol[8],
-                             soften_phy=sobol[8]*1.5,  # TODO check this
-                            )
+        param = param.format(
+            a_start=conf.a_start,
+            a_stop=1 + sobol[1],
+            box_size=conf.box_size[0],
+            M_g=conf.M * 1e3,
+            L_cm=conf.L * 1e2,
+            V_cm_s=conf.V * 1e2,
+            H_0=conf.H_0,
+            G=conf.G,
+            Omega_m=cosmo.Omega_m,
+            Omega_de=cosmo.Omega_de,
+            Omega_b=cosmo.Omega_b,
+            h=cosmo.h,
+            soften_cmv=sobol[8],
+            soften_phy=sobol[8] * 1.5,  # making this irrelevant
+        )
         fo.write(param)
 
     # times (scale factors) of output snapshots
     outtimes = np.linspace(1/16, 1, 121) + sobol[1]
-    np.savetxt(os.path.join(sim_dir, 'outtimes.txt'), outtimes, fmt='%.15f')
+    np.savetxt(os.path.join(sim_dir, 'outtimes.txt'), outtimes, fmt='%.16f')
 
     # slurm job script
     with (open(tpl_job, 'r') as f,
@@ -69,17 +70,16 @@ def gen_g4files(sim_dir, i,
 
 
 if __name__ == "__main__":
-    i_start, i_end = 0, 7
+    i_start, i_stop = sys.argv[1], sys.argv[2]
     base_dir = 'g4sims'
 
-    for i in range(i_start, i_end+1):
+    for i in range(i_start, i_stop):  # not including i_stop
         # create the sub-directories for each simulation
         sim_dir = os.path.join(base_dir, f'{i:03}')
-        if not os.path.exists(sim_dir):
-            os.makedirs(sim_dir)
+        os.makedirs(sim_dir, exist_ok=True)
 
         # generate ic and necessary files
         gen_g4files(sim_dir, i)
 
         # submit the job (compile + run)
-        # os.system(f'cd {sim_dir}; sbatch job.sh')
+        # os.system(f'cd {sim_dir} && sbatch job.sh')
