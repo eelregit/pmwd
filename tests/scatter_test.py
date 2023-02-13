2d34df0247cf806e69d82386b90254e1e2f3670e
Libin Lu <llu@flatironinstitute.org>
2023-02-13 00:24:19 -0500

    fixing crashing

---
+++ tests/scatter_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import jax.numpy as jnp
+from jax import device_put
+import pmwd
+
+print("loading")
+pmid = jnp.load("/mnt/ceph/users/yinli/scatter_gather_tests/pmid.npy")
+mesh = jnp.load("/mnt/ceph/users/yinli/scatter_gather_tests/mesh.npy")
+val = jnp.load("/mnt/ceph/users/yinli/scatter_gather_tests/val.npy")
+disp = jnp.load("/mnt/ceph/users/yinli/scatter_gather_tests/disp.npy")
+print("finish loading")
+
+print("casting")
+pmid = pmid.astype(jnp.uint32)
+print("finish casting")
+
+cell_size = 1.0
+ptcl_spacing = 1.0
+offset = tuple((0.0,0.0,0.))
+pmwd.scatter_cuda(pmid, disp, val, mesh, offset, ptcl_spacing, cell_size)
