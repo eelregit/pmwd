dc9fe03de2d7cb9367b28d2c6357aabd9fce99af
Libin Lu <llu@flatironinstitute.org>
2023-02-15 02:21:37 -0500

    more testing

--- tests/scatter_test.py
+++ tests/scatter_test.py
@@ -44,7 +44,7 @@ offset = tuple((0.0,0.0,0.))
 mesh0 = mesh*0;
 print(mesh0.unsafe_buffer_pointer())
 start = time.time()
-for ii in range(10000):
+for ii in range(20000):
     mesh0 = pmwd.scatter_cuda(pmid, disp, val, mesh0, offset, ptcl_spacing, cell_size).block_until_ready()
 print("time:")
 print(time.time() - start)
@@ -58,7 +58,7 @@ conf = Configuration(ptcl_spacing, ptcl_grid_shape, mesh_shape=1)  # 1x mesh sha
 mesh_val = mesh0*0
 
 start = time.time()
-for ii in range(10000):
+for ii in range(20000):
     mesh_val = _scatter(pmid, disp, conf, mesh_val, val, 0, cell_size).block_until_ready()
 print("time:")
 print(time.time() - start)
