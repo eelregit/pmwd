6ae9b840d1e4d1feffa4a03dbf76aeaa9750a9b3
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-03-01 11:26:48 +0800

    Add scaled sobol csv file for convenience

---
+++ docs/examples/sto/g4gen/scaled_sobol_csv.py
@@ -0,0 +1,16 @@
+"""Generate the csv file of scaled Sobol parameters for convenience."""
+import numpy as np
+from pmwd.train_util import scale_Sobol
+
+sobol = scale_Sobol()
+
+header = ','.join(['index', 'box size [Mpc]', 'offset Delta_a',
+                   'A_s_1e9', 'n_s', 'Omega_m', 'Omega_b',
+                   'Omega_k', 'h', 'softening length'])
+fmt = ['%d', '%.0f', '%.4f',
+       '%.2f', '%.2f', '%.2f', '%.3f',
+       '%.2f', '%.2f', '%.3f']
+
+np.savetxt('scaled_sobol_params.csv',
+           np.hstack((np.arange(sobol.shape[0]).reshape(-1, 1), sobol)),
+           delimiter=',', header=header, fmt=fmt, comments='')
