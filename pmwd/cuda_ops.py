7b9826b681b0bebe646bf99e657a369e1634542f
lu1and10 <llu@flatironinstitute.org>
2023-03-26 19:28:31 +0800

    adding general sort op python part

--- pmwd/cuda_ops.py
+++ pmwd/cuda_ops.py
@@ -21,7 +21,6 @@ for name, fn in _jaxpmwd.registrations().items():
 ### define scatter op
 @partial(jit, static_argnums=(4,5,6,7))
 def scatter_cuda(pmid, disp, val, mesh, offset, ptcl_grid, ptcl_spacing, cell_size):
-
     return _scatter_prim.bind(pmid, disp, val, mesh, offset=offset, ptcl_grid=ptcl_grid, ptcl_spacing=ptcl_spacing, cell_size=cell_size)
 
 def _scatter_abstract_eval(pmid, disp, val, mesh, offset, ptcl_grid, ptcl_spacing, cell_size):
@@ -97,7 +96,6 @@ mlir.register_lowering(_scatter_prim, _scatter_lowering, platform="gpu")
 ### define gather op
 @partial(jit, static_argnums=(4,5,6))
 def gather_cuda(pmid, disp, val, mesh, offset, ptcl_spacing, cell_size):
-
     return _gather_prim.bind(pmid, disp, val, mesh, offset=offset, ptcl_spacing=ptcl_spacing, cell_size=cell_size)
 
 def _gather_abstract_eval(pmid, disp, val, mesh, offset, ptcl_spacing, cell_size):
@@ -169,3 +167,77 @@ _gather_prim = Primitive("gather_cuda")
 _gather_prim.def_impl(partial(xla.apply_primitive, _gather_prim))
 _gather_prim.def_abstract_eval(_gather_abstract_eval)
 mlir.register_lowering(_gather_prim, _gather_lowering, platform="gpu")
+
+### define sort keys op
+@jit
+def sort_keys_cuda(keys):
+    return _sort_keys_prim.bind(keys)
+
+def _sort_keys_abstract_eval(keys):
+    return keys.update(shape=keys.shape, dtype=keys.dtype)
+
+def _sort_keys_lowering(ctx, keys, *, platform="gpu"):
+    # Extract the numpy type of the inputs
+    keys_aval, *_ = ctx.avals_in
+    out_aval, *_ = ctx.avals_out
+    np_dtype = np.dtype(keys_aval.dtype)
+    in_type = ir.RankedTensorType(keys.type)
+    in_layout = tuple(range(len(in_type.shape) - 1, -1, -1))
+    out_type = ir.RankedTensorType(keys.type)
+    out_layout = tuple(range(len(out_type.shape) - 1, -1, -1))
+
+    # We dispatch a different call depending on the dtype
+    if np_dtype == np.float32:
+        op_name = platform + "_sort_keys_f32"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_sort_keys_descriptor_f32(np.prod(in_type.shape).astype(np.int64))
+    elif np_dtype == np.float64:
+        op_name = platform + "_sort_keys_f64"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_sort_keys_descriptor_f64(np.prod(in_type.shape).astype(np.int64))
+    elif np_dtype == np.int32:
+        op_name = platform + "_sort_keys_f64"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_sort_keys_descriptor_i32(np.prod(in_type.shape).astype(np.int64))
+    elif np_dtype == np.int64:
+        op_name = platform + "_sort_keys_f64"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_sort_keys_descriptor_i64(np.prod(in_type.shape).astype(np.int64))
+    else:
+        raise NotImplementedError(f"Unsupported dtype {np_dtype}")
+
+    workspace = mlir.full_like_aval(ctx, 0, core.ShapedArray(shape=[workspace_size], dtype=np.byte))
+
+    # And then the following is what changes between the GPU and CPU
+    if platform == "cpu":
+        raise NotImplementedError(f"Unsupported cpu platform")
+    elif platform == "gpu":
+        if _jaxpmwd is None:
+            raise ValueError(
+                "The '_jaxpmwd' module was not compiled with CUDA support"
+            )
+
+        # TODO: if we use shared mem with bin sort, bin sort work mem allocate by XLA here and pass to cuda
+        result = custom_call(
+            op_name,
+            # Output types
+            out_types=[out_type],
+            # The inputs:
+            operands=[keys],
+            # Layout specification:
+            operand_layouts=[in_layout, (0,)],
+            result_layouts=[out_layout],
+            operand_output_aliases={0:0},
+            # GPU specific additional data
+            backend_config=opaque,
+        )
+        return hlo.ReshapeOp(mlir.aval_to_ir_type(out_aval), result).results
+
+    raise ValueError(
+        "Unsupported platform; this must be 'gpu'"
+    )
+
+_sort_keys_prim = Primitive("sort_keys_cuda")
+_sort_keys_prim.def_impl(partial(xla.apply_primitive, _sort_keys_prim))
+_sort_keys_prim.def_abstract_eval(_sort_keys_abstract_eval)
+mlir.register_lowering(_sort_keys_prim, _sort_keys_lowering, platform="gpu")
