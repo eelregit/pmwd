3b87380fed2631265a2a169615e64dc1c049cb5c
lu1and10 <llu@flatironinstitute.org>
2023-04-03 08:58:18 +0800

    adding enmesh cuda op jax part interface

--- pmwd/cuda_ops.py
+++ pmwd/cuda_ops.py
@@ -315,3 +315,80 @@ _argsort_prim = Primitive("argsort_cuda")
 _argsort_prim.def_impl(partial(xla.apply_primitive, _argsort_prim))
 _argsort_prim.def_abstract_eval(_argsort_abstract_eval)
 mlir.register_lowering(_argsort_prim, _argsort_lowering, platform="gpu")
+
+### define enmesh op
+@partial(jit, static_argnums=(3,4,5,6))
+def enmesh_cuda(pmid, disp, mesh, offset, ptcl_grid, ptcl_spacing, cell_size):
+    return _enmesh_prim.bind(pmid, disp, mesh, offset=offset, ptcl_grid=ptcl_grid, ptcl_spacing=ptcl_spacing, cell_size=cell_size)
+
+# returns: 1. index of argsort by cellid, 2. ptcl count of each cell, 3. dense edge of each cell
+def _enmesh_abstract_eval(pmid, disp, mesh, offset, ptcl_grid, ptcl_spacing, cell_size):
+    shape_mesh = [np.prod(mesh.shape)]
+    shape_ptcl = [np.prod(pmid.shape)]
+    return (ShapeArray(shape_ptcl, dtypes.canonicalize_dtype(np.uint32)), ShapeArray(shape_mesh, dtypes.canonicalize_dtype(np.uint32)), ShapeArray(shape_mesh, dtypes.canonicalize_dtype(np.uint32)))
+
+def _enmesh_lowering(ctx, pmid, disp, mesh, *, offset, ptcl_grid, ptcl_spacing, cell_size, platform="gpu"):
+    # Extract the numpy type of the inputs
+    pmid_aval, disp_aval, *_ = ctx.avals_in
+    ptcl_out_aval, mesh_out_aval, *_ = ctx.avals_out
+    np_dtype = np.dtype(disp_aval.dtype)
+    np_pmidtype = np.dtype(pmid_aval.dtype)
+    in_type1 = ir.RankedTensorType(pmid.type)
+    in_type2 = ir.RankedTensorType(mesh.type)
+    in_layout1 = tuple(range(len(in_type1.shape) - 1, -1, -1))
+    in_layout2 = tuple(range(len(in_type2.shape) - 1, -1, -1))
+
+    ptcl_out_type = ir.RankedTensorType.get(ptcl_out_aval.shape, ir.IntegerType.get_unsigned(32))
+    ptcl_out_layout = tuple(range(len(ptcl_out_type.shape) - 1, -1, -1))
+    mesh_out_type = ir.RankedTensorType.get(mesh_out_aval.shape, ir.IntegerType.get_unsigned(32))
+    mesh_out_layout = tuple(range(len(mesh_out_type.shape) - 1, -1, -1))
+
+    # todo pmid int type should be uint16, uint8 or uint32?
+    assert np_pmidtype == np.uint32
+
+    # We dispatch a different call depending on the dtype
+    if np_dtype == np.float32:
+        op_name = platform + "_enmesh_f32"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_enmesh_descriptor_f32(np.prod(in_type2.shape).astype(np.int64), ptcl_spacing, cell_size, *offset, *ptcl_grid, *out_type.shape)
+    elif np_dtype == np.float64:
+        op_name = platform + "_enmesh_f64"
+        # dimension using the 'opaque' parameter
+        workspace_size, opaque = _jaxpmwd.build_enmesh_descriptor_f64(np.prod(in_type2.shape).astype(np.int64), ptcl_spacing, cell_size, *offset, *ptcl_grid, *out_type.shape)
+    else:
+        raise NotImplementedError(f"Unsupported dtype {np_dtype}")
+
+    workspace = mlir.full_like_aval(ctx, 0, core.ShapedArray(shape=[workspace_size], dtype=np.byte))
+
+    # And then the following is what changes between the GPU and CPU
+    if platform == "cpu":
+        raise NotImplementedError(f"Unsupported cpu platform")
+    elif platform == "gpu":
+        if _jaxpmwd is None:
+            raise ValueError(
+                "The '_jaxpmwd' module was not compiled with CUDA support"
+            )
+
+        # TODO: if we use shared mem with bin sort, bin sort work mem allocate by XLA here and pass to cuda
+        result = custom_call(
+            op_name,
+            # Output types
+            out_types=[ptcl_out_type, mesh_out_type, mesh_out_type],
+            # The inputs:
+            operands=[pmid,disp,mesh,workspace],
+            # Layout specification:
+            operand_layouts=[in_layout1, in_layout1, in_layout2, (0,)],
+            result_layouts=[ptcl_out_layout, mesh_out_layout, mesh_out_layout],
+            # GPU specific additional data
+            backend_config=opaque,
+        )
+        return hlo.ReshapeOp(mlir.aval_to_ir_type(out_aval), result).results
+
+    raise ValueError(
+        "Unsupported platform; this must be 'gpu'"
+    )
+
+_enmesh_prim = Primitive("enmesh_cuda")
+_enmesh_prim.def_impl(partial(xla.apply_primitive, _enmesh_prim))
+_enmesh_prim.def_abstract_eval(_enmesh_abstract_eval)
+mlir.register_lowering(_enmesh_prim, _enmesh_lowering, platform="gpu")
