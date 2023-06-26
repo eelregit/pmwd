51b44fa607e2a098bc6ac6e6b298ca89f7d549fa
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-06-26 03:30:25 -0400

    Add a func for feature importance with rand forest

--- pmwd/sto/sr.py
+++ pmwd/sto/sr.py
@@ -1,4 +1,7 @@
 """Some util functions related to the Symbolic Regression."""
+import numpy as np
+from pysr import PySRRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 
 def gen_data_sr():
@@ -7,3 +10,13 @@ def gen_data_sr():
     X, y = None, None
     X_names = None
     return X, y, X_names
+
+
+def get_feature_importance(X, y, n_estimators=100, max_depth=3, random_state=None):
+    """Get the feature importance and indices, from high to low."""
+    clf = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth,
+                                random_state=random_state)
+    clf.fit(X, y)
+    idx = np.argsort(clf.feature_importances_)[::-1]  # high to low
+
+    return clf.feature_importances_[idx], idx
