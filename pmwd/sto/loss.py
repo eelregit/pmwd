46657e13678978fe42abee7f494c0ec8de0c693d
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-08-15 02:12:40 -0400

    FIXUP jit loss_snap to speed up training

--- pmwd/sto/loss.py
+++ pmwd/sto/loss.py
@@ -8,8 +8,7 @@ from pmwd.spec_util import powspec
 from pmwd.sto.util import scatter_dens, pv2ptcl
 
 
-@partial(jit, static_argnames=('log', 'norm'))
-def _loss_mse(f, g, log=True, norm=True, weights=None):
+def loss_mse(f, g, log=True, norm=True, weights=None):
     """MSE between two arrays, with optional modifications."""
     loss = jnp.abs(f - g)**2
 
@@ -29,7 +28,7 @@ def _loss_mse(f, g, log=True, norm=True, weights=None):
     return loss
 
 
-def _loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=True):
+def loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=True):
     # f (model) & g (target) are fields of the same shape in configuration space
     k, P_d, N, bins = powspec(f - g, spacing, w=w, cut_nyq=cut_nyq)
     k, P_g, N, bins = powspec(g, spacing, cut_nyq=cut_nyq)
@@ -39,44 +38,45 @@ def _loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=True):
     return loss
 
 
-def _loss_power_ln(f, g, spacing=1, cut_nyq=True):
+def loss_power_ln(f, g, spacing=1, cut_nyq=True):
     k, P_d, N, bins = powspec(f - g, spacing, cut_nyq=cut_nyq)
     k, P_g, N, bins = powspec(g, spacing, cut_nyq=cut_nyq)
     loss = jnp.log(P_d / P_g).sum() / len(k)
     return loss
 
 
-def loss_snap(ptcl, tgt, a, conf, loss_mesh_shape=1):
-
-    # get the target ptcl
-    ptcl_t = pv2ptcl(*tgt, ptcl.pmid, ptcl.conf)
-
+def loss_snap_disp(ptcl, ptcl_t, conf):
     # get the disp from particles' grid Lagrangian positions
     # may be necessary since we have it divided in the mse
     disp, disp_t = (ptcl_rpos(p, Particles.gen_grid(p.conf), p.conf)
                     for p in (ptcl, ptcl_t))
-    # disp = ptcl.disp
     # reshape -> make last 3 axes spatial dims
     shape_ = (-1,) + conf.ptcl_grid_shape
     disp = disp.T.reshape(shape_)
     disp_t = disp_t.T.reshape(shape_)
 
+    loss = loss_mse(disp, disp_t)
+    return loss
+
+
+def loss_snap_dens(ptcl, ptcl_t, conf, loss_mesh_shape):
     # get the density fields
-    (dens, dens_t), cell_size = scatter_dens(
-                                        (ptcl, ptcl_t), conf, loss_mesh_shape)
+    (dens, dens_t), cell_size = scatter_dens((ptcl, ptcl_t), conf, loss_mesh_shape)
 
+    # loss = loss_power_w(dens, dens_t)
+    loss = loss_power_ln(dens, dens_t)
+    return loss
+
+
+@jit
+def loss_snap(ptcl, ptcl_t, a, conf, loss_mesh_shape=1):
     loss = 0.
-
     # displacement
-    loss += _loss_mse(disp, disp_t)
-
+    loss += loss_snap_disp(ptcl, ptcl_t, conf)
     # density field
-    # loss += _loss_power_w(dens, dens_t)
-    loss += _loss_power_ln(dens, dens_t)
-
+    loss += loss_snap_dens(ptcl, ptcl_t, conf, loss_mesh_shape)
     # divided by the number of nbody steps to this snap
     loss /= (a - conf.a_start) // conf.a_nbody_step + 1
-
     return loss
 
 
@@ -85,7 +85,8 @@ def loss_func(obsvbl, tgts, conf):
 
     for tgt, a in zip(tgts, conf.a_snapshots):
         ptcl = obsvbl['snapshots'][a]
-        loss += loss_snap(ptcl, tgt, a, conf)
+        ptcl_t = pv2ptcl(*tgt, ptcl.pmid, ptcl.conf)  # get the target ptcl
+        loss += loss_snap(ptcl, ptcl_t, a, conf)
     loss /= len(tgts)  # mean loss per snapshot
 
     return loss
