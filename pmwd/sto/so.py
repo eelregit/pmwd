0c067f8174bc0a9cb436cfd433e88254f48554b4
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-08-18 04:59:51 -0400

    FIXUP refactor so features

--- pmwd/sto/so.py
+++ pmwd/sto/so.py
@@ -1,92 +1,22 @@
-import jax
 import jax.numpy as jnp
 from jax import vmap
-from jax.tree_util import tree_map
 import math
 
-from pmwd.configuration import Configuration
-from pmwd.cosmology import Cosmology, H_deriv, Omega_m_a, SimpleLCDM
-from pmwd.boltzmann import boltzmann, growth, linear_power
 from pmwd.sto.mlp import MLP
-
-
-def nonlinear_scales(cosmo, conf, a):
-    k = conf.transfer_k[1:]
-    D = growth(a, cosmo, conf)
-    dD = growth(a, cosmo, conf, deriv=1)
-    dD2i = -2 * D**(-3) * dD  # d(1/D^2) / dlna
-    interp_valgrad = jax.value_and_grad(jnp.interp, argnums=0)
-
-    # dimensionless linear power
-    Plin = linear_power(k, None, cosmo, conf)  # no a dependence
-    k_P, dk_P = interp_valgrad(1 / D**2, k**3 * Plin / (2 * jnp.pi**2), k)
-    dk_P *= dD2i
-
-    # TopHat variance, var is decreasing with R
-    # but for jnp.interp, xp must be increasing, thus the reverse [::-1]
-    R_TH, dR_TH = interp_valgrad(1 / D**2, cosmo.varlin[::-1], conf.varlin_R[::-1])
-    dR_TH *= dD2i
-
-    # Gaussian variance
-    R_G, dR_G = interp_valgrad(1 / D**2, cosmo.varlin_g[::-1], conf.varlin_R_g[::-1])
-    dR_G *= dD2i
-
-    # rms linear theory displacement
-    Rd = (jnp.trapz(k * Plin, x=jnp.log(k)) + k[0] * Plin[0] / 2) / (6 * jnp.pi**2)
-    Rd = jnp.sqrt(Rd)
-    dRd = Rd * dD
-    Rd *= D
-
-    return (1/k_P, R_TH, R_G, Rd, -dk_P/k_P**2, dR_TH, dR_G, dRd)
+from pmwd.sto.soft import (
+    _sotheta, _sotheta_names, _soft_len, soft, soft_k, soft_kvec)
 
 
 def sotheta(cosmo, conf, a):
-    # quantities of dim L
-    theta_l = jnp.asarray([
-        *nonlinear_scales(cosmo, conf, a),
-        conf.ptcl_spacing,
-        conf.cell_size,
-        conf.softening_length,
-    ])
-
-    # dimensionless quantities
-    D1 = growth(a, cosmo, conf, order=1)
-    dlnD1 = growth(a, cosmo, conf, order=1, deriv=1) / D1
-    D2 = growth(a, cosmo, conf, order=2)
-    dlnD2 = growth(a, cosmo, conf, order=2, deriv=1) / D2
-    theta_o = jnp.asarray([
-        D1 / a,
-        D2 / a**2,
-        dlnD1 - 1,
-        dlnD2 - 2,
-        Omega_m_a(a, cosmo),
-        H_deriv(a, cosmo),
-        conf.a_nbody_step / a,  # time step size dlna ~ da/a
-    ])
-
-    return (theta_l, theta_o)
+    return _sotheta(cosmo, conf, a)
 
 
 def sotheta_names():
-    # str names of the variables returned by the sotheta function above
-    # currently hardcoded, should be updated along with sotheta function
-    pass
+    return _sotheta_names()
 
 
-def soft_len(l_fac=1):
-    # get the length of SO input features with dummy conf and cosmo
-    conf = Configuration(1., (128,)*3)
-    cosmo = SimpleLCDM(conf)
-    cosmo = boltzmann(cosmo, conf)
-    theta_l, theta_o = sotheta(cosmo, conf, conf.a_start)
-    return len(theta_l) * l_fac + len(theta_o)
-
-
-def soft(k, theta):
-    """SO features for neural nets input, with k being a scalar."""
-    theta_l, theta_o = theta
-    k_theta_l = k * theta_l
-    return jnp.concatenate((k_theta_l, theta_o))
+def soft_len(k_fac=1):
+    return _soft_len(k_fac=k_fac)
 
 
 def apply_net(nid, conf, cosmo, x):
@@ -109,40 +39,14 @@ def sonn_vmap(k, theta, cosmo, conf, nid):
     return vmap(_sonn)(k.ravel()).reshape(k.shape)
 
 
-def soft_k(k, theta):
-    """Get SO input features (k * l, o)."""
-    theta_l, theta_o = theta  # e.g. (8,), (6,)
-    k_shape = k.shape  # e.g. (128, 1, 1)
-    k = k.reshape(k_shape + (1,))  # (128, 1, 1, 1)
-    theta_l = theta_l.reshape((1,) * len(k_shape) + theta_l.shape)  # (1, 1, 1, 8)
-    ft = k * theta_l  # (128, 1, 1, 8)
-    theta_o = jnp.broadcast_to(theta_o, k_shape+theta_o.shape)  # (128, 1, 1, 6)
-    ft = jnp.concatenate((ft, theta_o), axis=-1)  # (128, 1, 1, 8+6)
-    return ft
-
-
 def sonn_k(k, theta, cosmo, conf, nid):
-    """SO net with input: (k * l, o)."""
+    """SO net with 1D k input."""
     ft = soft_k(k, theta)
     return apply_net(nid, conf, cosmo, ft)[..., 0]  # rm the trailing axis of dim one
 
 
-def soft_kvec(kv, theta):
-    """Get SO input features (k1 * l, k2 * l, k3 * l, o)."""
-    kv_shape = kv.shape  # e.g. (128, 128, 65, 3)
-    kv = kv.reshape(kv_shape + (1,))  # (128, 128, 65, 3, 1)
-
-    theta_l, theta_o = theta  # e.g. (8,), (6,)
-    theta_l = theta_l.reshape((1,) * len(kv_shape) + theta_l.shape)  # (1, 1, 1, 1, 8)
-    ft = kv * theta_l  # (128, 128, 65, 3, 8)
-    ft = ft.reshape(kv_shape[:-1] + (-1,))  # (128, 128, 65, 3*8)
-    theta_o = jnp.broadcast_to(theta_o, kv_shape[:-1]+theta_o.shape)  # (128, 128, 65, 6)
-    ft = jnp.concatenate((ft, theta_o), axis=-1)  # (128, 128, 65, 3*8+6)
-    return ft
-
-
 def sonn_kvec(kv, theta, cosmo, conf, nid):
-    """SO net with input: (k1 * l, k2 * l, k3 * l, o)."""
+    """SO net with 3D k input."""
     ft = soft_kvec(kv, theta)
     return apply_net(nid, conf, cosmo, ft)[..., 0]  # rm the trailing axis of dim one
 
---
+++ pmwd/sto/soft.py
@@ -0,0 +1,113 @@
+import jax
+import jax.numpy as jnp
+
+from pmwd.configuration import Configuration
+from pmwd.cosmology import Cosmology, H_deriv, Omega_m_a, SimpleLCDM
+from pmwd.boltzmann import boltzmann, growth, linear_power
+
+
+def nonlinear_scales(cosmo, conf, a):
+    """Some nonlinear scales and their time derivatives."""
+    k = conf.transfer_k[1:]
+    D = growth(a, cosmo, conf)
+    dD = growth(a, cosmo, conf, deriv=1)
+    dD2i = -2 * D**(-3) * dD  # d(1/D^2) / dlna
+    interp_valgrad = jax.value_and_grad(jnp.interp, argnums=0)
+
+    # dimensionless linear power
+    Plin = linear_power(k, None, cosmo, conf)  # no a dependence
+    k_P, dk_P = interp_valgrad(1 / D**2, k**3 * Plin / (2 * jnp.pi**2), k)
+    dk_P *= dD2i
+
+    # TopHat variance, var is decreasing with R
+    # but for jnp.interp, xp must be increasing, thus the reverse [::-1]
+    R_TH, dR_TH = interp_valgrad(1 / D**2, cosmo.varlin[::-1], conf.varlin_R[::-1])
+    dR_TH *= dD2i
+
+    # Gaussian variance
+    R_G, dR_G = interp_valgrad(1 / D**2, cosmo.varlin_g[::-1], conf.varlin_R_g[::-1])
+    dR_G *= dD2i
+
+    # rms linear theory displacement
+    Rd = (jnp.trapz(k * Plin, x=jnp.log(k)) + k[0] * Plin[0] / 2) / (6 * jnp.pi**2)
+    Rd = jnp.sqrt(Rd)
+    dRd = Rd * dD
+    Rd *= D
+
+    return (1/k_P, R_TH, R_G, Rd, -dk_P/k_P**2, dR_TH, dR_G, dRd)
+
+
+def _sotheta(cosmo, conf, a):
+    """Physical quantities to be used in SO input features along with k."""
+    # quantities of dim L
+    theta_l = jnp.asarray([
+        *nonlinear_scales(cosmo, conf, a),
+        conf.ptcl_spacing,
+        conf.cell_size,
+        conf.softening_length,
+    ])
+
+    # dimensionless quantities
+    D1 = growth(a, cosmo, conf, order=1)
+    dlnD1 = growth(a, cosmo, conf, order=1, deriv=1) / D1
+    D2 = growth(a, cosmo, conf, order=2)
+    dlnD2 = growth(a, cosmo, conf, order=2, deriv=1) / D2
+    theta_o = jnp.asarray([
+        D1 / a,
+        D2 / a**2,
+        dlnD1 - 1,
+        dlnD2 - 2,
+        Omega_m_a(a, cosmo),
+        H_deriv(a, cosmo),
+        conf.a_nbody_step / a,  # time step size dlna ~ da/a
+    ])
+
+    return (theta_l, theta_o)
+
+
+def _sotheta_names():
+    # str names of the variables returned by the sotheta function above
+    # currently hardcoded, should be updated along with sotheta function
+    pass
+
+
+def _soft_len(k_fac=1):
+    # get the length of SO input features with dummy conf and cosmo
+    conf = Configuration(1., (128,)*3)
+    cosmo = SimpleLCDM(conf)
+    cosmo = boltzmann(cosmo, conf)
+    theta_l, theta_o = _sotheta(cosmo, conf, conf.a_start)
+    return len(theta_l) * k_fac + len(theta_o)
+
+
+def soft(k, theta):
+    """SO features for neural nets input, with k being a scalar."""
+    theta_l, theta_o = theta
+    k_theta_l = k * theta_l
+    return jnp.concatenate((k_theta_l, theta_o))
+
+
+def soft_k(k, theta):
+    """Get SO input features (k * l, o)."""
+    theta_l, theta_o = theta  # e.g. (8,), (6,)
+    k_shape = k.shape  # e.g. (128, 1, 1)
+    k = k.reshape(k_shape + (1,))  # (128, 1, 1, 1)
+    theta_l = theta_l.reshape((1,) * len(k_shape) + theta_l.shape)  # (1, 1, 1, 8)
+    ft = k * theta_l  # (128, 1, 1, 8)
+    theta_o = jnp.broadcast_to(theta_o, k_shape+theta_o.shape)  # (128, 1, 1, 6)
+    ft = jnp.concatenate((ft, theta_o), axis=-1)  # (128, 1, 1, 8+6)
+    return ft
+
+
+def soft_kvec(kv, theta):
+    """Get SO input features (k1 * l, k2 * l, k3 * l, o)."""
+    kv_shape = kv.shape  # e.g. (128, 128, 65, 3)
+    kv = kv.reshape(kv_shape + (1,))  # (128, 128, 65, 3, 1)
+
+    theta_l, theta_o = theta  # e.g. (8,), (6,)
+    theta_l = theta_l.reshape((1,) * len(kv_shape) + theta_l.shape)  # (1, 1, 1, 1, 8)
+    ft = kv * theta_l  # (128, 128, 65, 3, 8)
+    ft = ft.reshape(kv_shape[:-1] + (-1,))  # (128, 128, 65, 3*8)
+    theta_o = jnp.broadcast_to(theta_o, kv_shape[:-1]+theta_o.shape)  # (128, 128, 65, 6)
+    ft = jnp.concatenate((ft, theta_o), axis=-1)  # (128, 128, 65, 3*8+6)
+    return ft
