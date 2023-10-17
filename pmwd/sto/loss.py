281ee42f922386c3afc3baf0c8532545a995f6b5
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-10-16 23:14:24 -0400

    FIXUP loss with modified obsvbl

--- pmwd/sto/loss.py
+++ pmwd/sto/loss.py
@@ -1,5 +1,6 @@
 from jax import jit
 import jax.numpy as jnp
+from jax.lax import scan
 from functools import partial
 
 from pmwd.particles import Particles, ptcl_rpos
@@ -28,7 +29,7 @@ def loss_mse(f, g, log=True, norm=True, weights=None):
     return loss
 
 
-def loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=True):
+def loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=False):
     # f (model) & g (target) are fields of the same shape in configuration space
     k, P_d, N, bins = powspec(f - g, spacing, w=w, cut_nyq=cut_nyq)
     k, P_g, N, bins = powspec(g, spacing, cut_nyq=cut_nyq)
@@ -38,7 +39,7 @@ def loss_power_w(f, g, spacing=1, log=True, w=None, cut_nyq=True):
     return loss
 
 
-def loss_power_ln(f, g, spacing=1, cut_nyq=True):
+def loss_power_ln(f, g, spacing=1, cut_nyq=False):
     k, P_d, N, bins = powspec(f - g, spacing, cut_nyq=cut_nyq)
     k, P_g, N, bins = powspec(g, spacing, cut_nyq=cut_nyq)
     loss = jnp.log(P_d / P_g).sum() / len(k)
@@ -76,17 +77,28 @@ def loss_snap(ptcl, ptcl_t, a, conf, loss_mesh_shape=1):
     # density field
     loss += loss_snap_dens(ptcl, ptcl_t, conf, loss_mesh_shape)
     # divided by the number of nbody steps to this snap
-    loss /= (a - conf.a_start) // conf.a_nbody_step + 1
+    # loss /= (a - conf.a_start) // conf.a_nbody_step + 1
     return loss
 
 
 def loss_func(obsvbl, tgts, conf):
     loss = 0.
 
-    for tgt, a in zip(tgts, conf.a_snapshots):
-        ptcl = obsvbl['snapshots'][a]
+    # TODO adapt the structure of tgts for scan
+    # def f_loss(carry, x):
+    #     loss = carry
+    #     tgt, a_snap, snap = x
+    #     ptcl_t = pv2ptcl(*tgt, ptcl.pmid, ptcl.conf)
+    #     loss += loss_snap(ptcl, ptcl_t, a_snap, conf)
+    #     return loss, None
+
+    # loss = scan(f_loss, loss, (tgts, obsvbl['a_snaps'], obsvbl['snaps']))[0]
+    # loss /= len(tgts)
+
+    for i, (tgt, a_snap) in enumerate(zip(tgts, obsvbl['a_snaps'])):
+        ptcl = obsvbl['snaps'][i]
         ptcl_t = pv2ptcl(*tgt, ptcl.pmid, ptcl.conf)  # get the target ptcl
-        loss += loss_snap(ptcl, ptcl_t, a, conf)
+        loss += loss_snap(ptcl, ptcl_t, a_snap, conf)
     loss /= len(tgts)  # mean loss per snapshot
 
     return loss
