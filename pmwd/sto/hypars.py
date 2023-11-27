44a672a955125da3270fc1b79bc0a4ca0e834ca7
Yucheng Zhang <yczhang.nyu@gmail.com>
2023-11-27 01:56:04 -0500

    FIXUP add loading existing optimizer state

--- pmwd/sto/hypars.py
+++ pmwd/sto/hypars.py
@@ -6,7 +6,7 @@ import pickle
 from pmwd.sto.so import soft_len
 from pmwd.sto.mlp import init_mlp_params
 
-n_epochs = 600
+n_epochs = 1000
 
 # data
 sobol_ids_global = np.arange(0, 64)
@@ -15,13 +15,31 @@ shuffle_epoch = True  # shuffle the order of sobols across epochs
 
 # optimizer
 learning_rate = 3e-5
-optimizer = optax.adam(learning_rate, b1=0.9, b2=0.9)
+optimizer = optax.adam(learning_rate)
 # optimizer = optax.chain(
 #     optax.clip_by_global_norm(3.),
 #     optax.adam(learning_rate),
 # )
 # optimizer = optax.amsgrad(learning_rate, eps=1e-4)
 # optimizer = optax.adamw(learning_rate, weight_decay=1e-4)
+
+# so neural nets
+so_type = 'NN'
+n_input = [soft_len(k_fac=3), soft_len()]
+
+so_nodes = [[3*n, 3*n, 3*n, 1] for n in n_input]
+
+# start a new training
+# so_params = init_mlp_params(n_input, so_nodes, scheme='last_ws_b1')
+# opt_state = optimizer.init(so_params)
+
+# load and continue a training
+with open('params/3016932/e1000.pickle', 'rb') as f:
+    dic = pickle.load(f)
+    so_params = dic['so_params']
+    opt_state = dic['opt_state']
+
+
 
 def get_optimizer(lr):
     return optax.adam(lr)
@@ -47,16 +65,6 @@ def lr_scheduler(lr, skd_state, loss, patience=15, factor=0.5, threshold=1e-4, l
     skd_state = (counter, loss_low)
 
     return lr, skd_state
-
-
-# so neural nets
-so_type = 'NN'
-n_input = [soft_len(k_fac=3), soft_len()]
-
-so_nodes = [[3*n, 3*n, 3*n, 1] for n in n_input]
-so_params = init_mlp_params(n_input, so_nodes, scheme='last_ws_b1')
-# with open('params/2980166/e107.pickle', 'rb') as f:
-#     so_params = pickle.load(f)['so_params']
 
 dropout_rate = None  # set to None for no dropout applied
 
