849cc772638bd207825adc7feaf2dd4cc9b4d642
Yin Li <eelregit@gmail.com>
2025-03-01 23:00:15 +0800

    Refactor and Rewrite binning to avoid jax/issues/25847

--- pmwd/spec_util.py
+++ pmwd/spec_util.py
@@ -1,15 +1,55 @@
 from functools import partial
 import math
 
-from jax import jit, ensure_compile_time_eval
+from jax import jit
 import jax.numpy as jnp
 
 from pmwd.pm_util import fftfreq, fftfwd
 
 
+def _getbins(grid_shape, bins, cut_nyq):
+    kfun = 1 / max(grid_shape)
+    knyq = 0.5
+    kmax = knyq * math.sqrt(3)
+
+    # first bin only contains the DC mode, and thus the "1 + " in front of bnum and bcut
+    if isinstance(bins, (int, float)):
+        bins *= kfun
+        bnum = 1 + math.ceil(kmax / bins)
+        bcut = 1 + math.ceil(knyq / bins) if cut_nyq else bnum
+        bins *= jnp.arange(bnum)
+        right = True
+
+    elif isinstance(bins, complex):
+        kmaxable = all(s % 2 == 0 for s in grid_shape)  # extra bin just in case
+        bnum = 1 + math.ceil(math.log2(kmax / kfun) / bins.imag) + kmaxable
+        bcut = 1 + math.ceil(math.log2(knyq / kfun) / bins.imag) if cut_nyq else bnum
+        bins = kfun * 2 ** (bins.imag * jnp.arange(bnum))
+        right = False
+
+    elif isinstance(bins, tuple):
+        if bins[0] != 0:
+            raise ValueError(f'{bins=} must starts from 0')
+
+        bnum = len(bins)
+        if cut_nyq:
+            for bcut, edge in enumerate(bins, start=1):
+                if edge >= knyq:
+                    break
+        else:
+            bcut = bnum
+        bins = jnp.asarray(bins)
+        right = True
+
+    else:
+        raise ValueError(f'{bins=} not supported')
+
+    return bnum, bcut, bins, right
+
+
 @partial(jit, static_argnames=('bins', 'cut_zero', 'cut_nyq', 'dtype', 'int_dtype'))
 def powspec(f, spacing, bins=1j/3, g=None, deconv=None, cut_zero=True, cut_nyq=True,
-            dtype=jnp.float_, int_dtype=jnp.uint32):
+            dtype=jnp.float64, int_dtype=jnp.uint32):
     """Compute auto or cross power spectrum in 3D averaged in spherical bins.
 
     Parameters
@@ -20,18 +60,19 @@ def powspec(f, spacing, bins=1j/3, g=None, deconv=None, cut_zero=True, cut_nyq=T
         Field grid spacing.
     bins : float, complex, or tuple, optional
         (Angular) wavenumber bins. A real number sets the linear bin width in unit of
-        the smallest fundamental in 3D (right edge inclusive starting from zero); an
-        imaginary number sets the log bin width in octave (left edge inclusive starting
-        from the smallest fundamental in 3D); and a tuple sets the bin edges directly
-        (right edge inclusive and must starting from zero).
+        the smallest fundamental in 3D (right-edge inclusive and starting from 0); an
+        imaginary number sets the log bin width in octave (left-edge inclusive and
+        starting from the smallest fundamental in 3D); and a tuple sets the bin edges
+        directly (increasing, right-edge inclusive, and must starts from 0) in unit of
+        2x the Nyquist frequency (such that ``cut_nyq`` cuts beyond 0.5).
     g : ArrayLike, optional
         Another field of the same shape for cross correlation.
     deconv : int, optional
         Power of sinc factors to deconvolve in the power spectrum.
     cut_zero : bool, optional
-        Whether to discard the bin containing the zero or DC mode.
+        Whether to discard the bin containing the k=0 or DC mode.
     cut_nyq : bool, optional
-        Whether to discard the bins beyond the Nyquist, only for linear or log ``bins``.
+        Whether to discard the bins beyond the Nyquist frequency.
     dtype : DTypeLike, optional
         Float dtype for the wavenumber and power spectrum.
     int_dtype : DTypeLike, optional
@@ -55,28 +96,7 @@ def powspec(f, spacing, bins=1j/3, g=None, deconv=None, cut_zero=True, cut_nyq=T
         raise ValueError(f'shape mismatch: {f.shape} != {jnp.shape(g)}')
     grid_shape = f.shape[-3:]
 
-    with ensure_compile_time_eval():
-        kfun = 1 / max(grid_shape)
-        knyq = 0.5
-        kmax = knyq * math.sqrt(3)
-        if isinstance(bins, (int, float)):
-            bins *= kfun
-            bin_num = math.ceil(kmax / bins)
-            bins *= jnp.arange(1 + bin_num)
-            right = True
-            bcut = jnp.digitize(knyq if cut_nyq else kmax, bins, right=right).item() + 1
-        elif isinstance(bins, complex):
-            kmaxable = all(s % 2 == 0 for s in grid_shape)  # extra bin just in case
-            bin_num = math.ceil(math.log2(kmax / kfun) / bins.imag) + kmaxable
-            bins = kfun * 2 ** (bins.imag * jnp.arange(1 + bin_num))
-            right = False
-            bcut = jnp.digitize(knyq if cut_nyq else kmax, bins, right=right).item() + 1
-        else:
-            bin_num = len(bins) - 1
-            bins = jnp.asarray(bins)
-            bins *= spacing / (2 * jnp.pi)  # convert to 2π spacing
-            right = True
-            bcut = bin_num + 1  # no trim otherwise hard to jit
+    bnum, bcut, bins, right = _getbins(grid_shape, bins, cut_nyq)
 
     last_three = range(-3, 0)
     f = fftfwd(f, axes=last_three)
@@ -108,9 +128,9 @@ def powspec(f, spacing, bins=1j/3, g=None, deconv=None, cut_zero=True, cut_nyq=T
     b = jnp.digitize(k, bins, right=right)
     k = (k * N).astype(dtype)
     P = (P * N).astype(dtype)  # FIXME after google/jax/issues/18440
-    k = jnp.bincount(b, weights=k, length=1+bin_num)  # only k=0 goes to b=0
-    P = jnp.bincount(b, weights=P, length=1+bin_num)
-    N = jnp.bincount(b, weights=N, length=1+bin_num)
+    k = jnp.bincount(b, weights=k, length=bnum)  # only k=0 goes to b=0
+    P = jnp.bincount(b, weights=P, length=bnum)
+    N = jnp.bincount(b, weights=N, length=bnum)
 
     k = k[cut_zero:bcut]
     P = P[cut_zero:bcut]
@@ -120,8 +140,8 @@ def powspec(f, spacing, bins=1j/3, g=None, deconv=None, cut_zero=True, cut_nyq=T
     k /= N
     P /= N
 
-    k *= 2 * jnp.pi / spacing
-    bins *= 2 * jnp.pi / spacing
+    k *= 2 * math.pi / spacing
+    bins *= 2 * math.pi / spacing
     P *= spacing**3 / math.prod(grid_shape)
 
     return k, P, N, bins
