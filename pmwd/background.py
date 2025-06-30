7d6874878eb7c47130ab6ed80e74f124dce359da
Yin Li <eelregit@gmail.com>
2025-06-30 14:30:25 +0800

    Refactor SK out of distance

--- pmwd/background.py
+++ pmwd/background.py
@@ -124,6 +124,35 @@ def distance_cache(cosmo):
     return cT
 
 
+def SK(chi, cosmo):
+    r"""Convert radial comoving distances to transverse ones.
+
+    Parameters
+    ----------
+    chi : ArrayLike
+        Radial/line-of-sight comoving distances in :math:`L`.
+    cosmo : Cosmology
+
+    Returns
+    -------
+    r : jax.Array
+        Transverse comoving / comoving angular diameter distances in :math:`L`.
+
+    Notes
+    -----
+    .. math::
+
+        r = \frac{S_K(\sqrt{|K|} \chi)}{\sqrt{|K|}},
+
+    where :math:`K` is `Cosmology.K`, and :math:`S_K` is sine, identity, or hyperbolic
+    sine for positive, zero, or negative :math:`K`, respectively.
+
+    """
+    branches = _SK_closed, _SK_flat, _SK_open
+    Ksqrt = jnp.sqrt(jnp.abs(cosmo.K))
+    r = switch(jnp.int8(jnp.sign(cosmo.Omega_K)) + 1, branches, chi, Ksqrt)
+    return r
+
 def _SK_closed(chi, Ksqrt):
     return jnp.sin(Ksqrt * chi) / Ksqrt
 
@@ -147,14 +176,15 @@ def distance(a, cosmo, type='radial', a_ref=1):
     type : {'light' or 0, 'radial' or 1, 'transverse', 'angdiam', 'luminosity', 'super'
             or 2, 'coldens' or 3}, optional
         Type of distances or times to return, among physical/light-travel distance,
-        radial/line-of-sight comoving distance, transverse comoving distance, angular
-        diameter distance, luminosity distance, supercomoving / superconformal /
-        dispersion measure distance, and that related to non-relativistic particle
-        column density.
+        radial/line-of-sight comoving distance, transverse comoving / comoving angular
+        diameter distance, angular diameter distance, luminosity distance, supercomoving
+        / superconformal / dispersion measure distance, and that related to
+        non-relativistic particle column density.
     time : bool, optional
         Whether to divide by the speed of light to return time measure instead, e.g.,
         for physical/lookback time with ``type='light'`` or conformal time with
-        ``type='radial'``.
+        ``type='radial'``. This has no effect on the transverse distances, i.e.,
+        'transverse', 'angdiam', and 'luminosity'.
     a_ref : ArrayLike, optional
         Scale factors of references.
 
@@ -177,14 +207,8 @@ def distance(a, cosmo, type='radial', a_ref=1):
     related to dispersion measure, and related to non-relativistic particle column
     density, respectively. So :math:`T_0 = t` and :math:`cT_1 = \chi`.
 
-    The transverse comoving or comoving angular diameter distance
-
-    .. math::
-
-        r = \frac{S_K(\sqrt{|K|} \chi)}{\sqrt{|K|}},
-
-    where :math:`S_K` is sine, identity, or hyperbolic sine for positive, zero, or
-    negative :math:`K`, respectively.
+    See `SK` for the transverse comoving or comoving angular diameter distance
+    :math:`r`.
 
     The angular diameter distance and luminosity distance
 
@@ -220,15 +244,12 @@ def distance(a, cosmo, type='radial', a_ref=1):
     d_ref = jnp.interp(a_ref, cosmo.distance_a, cosmo.distance[n])
     d -= d_ref
 
-    if time:
-        d /= cosmo.c
-
     if type in {'light', 'radial', 'super', 'coldens'}:
+        if time:
+            d /= cosmo.c
         return d
 
-    branches = _SK_closed, _SK_flat, _SK_open
-    Ksqrt = jnp.sqrt(jnp.abs(cosmo.K))
-    d = switch(jnp.int8(jnp.sign(cosmo.Omega_K)) + 1, branches, d, Ksqrt)
+    d = SK(d, cosmo)
     if type == 'transverse':
         return d
     if type == 'angdiam':
