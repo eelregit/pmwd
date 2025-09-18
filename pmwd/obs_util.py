fc464e6d930f31df1daca627b7aed54e3690a533
Yucheng Zhang <yczhang.nyu@gmail.com>
2025-09-18 15:05:09 +0800

    FIXUP dtype cast in obs_util

--- pmwd/obs_util.py
+++ pmwd/obs_util.py
@@ -8,22 +8,24 @@ from pmwd.cosmology import E2
 def itp_prev(ptcl, a0, a1, a, cosmo):
     """Cubic Hermite interpolation is a linear combination of two ptcls, this
        function returns the disp and vel from the first ptcl at a0."""
+    dtype = cosmo.conf.float_dtype
     Da = a1 - a0
     t = (a - a0) / Da
     a3E0 = a0**3 * jnp.sqrt(E2(a0, cosmo))
     # displacement
     h00 = 2 * t**3 - 3 * t**2 + 1
     h10 = t**3 - 2 * t**2 + t
-    disp = h00 * ptcl.disp + h10 * Da / a3E0 * ptcl.vel
+    disp = (h00.astype(dtype) * ptcl.disp +
+            (h10 * Da / a3E0).astype(dtype) * ptcl.vel)
     # velocity
     # derivatives of the Hermite basis functions
     h00 = 6 * t**2 - 6 * t
     h10 = 3 * t**2 - 4 * t + 1
-    vel = h00 / Da * ptcl.disp + h10 / a3E0 * ptcl.vel
-    vel *= a**3 * jnp.sqrt(E2(a, cosmo))
+    vel = ((h00 / Da).astype(dtype) * ptcl.disp +
+           (h10 / a3E0).astype(dtype) * ptcl.vel)
+    vel *= (a**3 * jnp.sqrt(E2(a, cosmo))).astype(dtype)
 
-    dtype = ptcl.conf.float_dtype
-    return disp.astype(dtype), vel.astype(dtype)
+    return disp, vel
 
 
 def itp_prev_adj(ptcl_cot, cosmo_cot, iptcl_cot, ptcl, a0, a1, a, cosmo):
@@ -43,22 +45,24 @@ def itp_prev_adj(ptcl_cot, cosmo_cot, iptcl_cot, ptcl, a0, a1, a, cosmo):
 def itp_next(ptcl, a0, a1, a, cosmo):
     """Cubic Hermite interpolation is a linear combination of two ptcls, this
        function returns the disp and vel from the second ptcl at a1."""
+    dtype = cosmo.conf.float_dtype
     Da = a1 - a0
     t = (a - a0) / Da
     a3E1 = a1**3 * jnp.sqrt(E2(a1, cosmo))
     # displacement
     h01 = - 2 * t**3 + 3 * t**2
     h11 = t**3 - t**2
-    disp = h01 * ptcl.disp + h11 * Da / a3E1 * ptcl.vel
+    disp = (h01.astype(dtype) * ptcl.disp +
+            (h11 * Da / a3E1).astype(dtype) * ptcl.vel)
     # velocity
     # derivatives of the Hermite basis functions
     h01 = - 6 * t**2 + 6 * t
     h11 = 3 * t**2 - 2 * t
-    vel = h01 / Da * ptcl.disp + h11 / a3E1 * ptcl.vel
-    vel *= a**3 * jnp.sqrt(E2(a, cosmo))
+    vel = ((h01 / Da).astype(dtype) * ptcl.disp +
+           (h11 / a3E1).astype(dtype) * ptcl.vel)
+    vel *= (a**3 * jnp.sqrt(E2(a, cosmo))).astype(dtype)
 
-    dtype = ptcl.conf.float_dtype
-    return disp.astype(dtype), vel.astype(dtype)
+    return disp, vel
 
 
 def itp_next_adj(ptcl_cot, cosmo_cot, iptcl_cot, ptcl, a0, a1, a, cosmo):
@@ -78,6 +82,7 @@ def itp_next_adj(ptcl_cot, cosmo_cot, iptcl_cot, ptcl, a0, a1, a, cosmo):
 def interptcl(ptcl0, ptcl1, a0, a1, a, cosmo):
     """Given two ptcl snapshots, get the interpolated one at a given time using
        cubic Hermite interpolation."""
+    dtype = cosmo.conf.float_dtype
     Da = a1 - a0
     t = (a - a0) / Da
     a3E0 = a0**3 * jnp.sqrt(E2(a0, cosmo))
@@ -87,17 +92,21 @@ def interptcl(ptcl0, ptcl1, a0, a1, a, cosmo):
     h10 = t**3 - 2 * t**2 + t
     h01 = - 2 * t**3 + 3 * t**2
     h11 = t**3 - t**2
-    disp = (h00 * ptcl0.disp + h10 * Da / a3E0 * ptcl0.vel +
-            h01 * ptcl1.disp + h11 * Da / a3E1 * ptcl1.vel)
+    disp = (h00.astype(dtype) * ptcl0.disp +
+            (h10 * Da / a3E0).astype(dtype) * ptcl0.vel +
+            h01.astype(dtype) * ptcl1.disp +
+            (h11 * Da / a3E1).astype(dtype) * ptcl1.vel)
     # velocity
     # derivatives of the Hermite basis functions
     h00 = 6 * t**2 - 6 * t
     h10 = 3 * t**2 - 4 * t + 1
     h01 = - 6 * t**2 + 6 * t
     h11 = 3 * t**2 - 2 * t
-    vel = (h00 / Da * ptcl0.disp + h10 / a3E0 * ptcl0.vel +
-           h01 / Da * ptcl1.disp + h11 / a3E1 * ptcl1.vel)
-    vel *= a**3 * jnp.sqrt(E2(a, cosmo))
+    vel = ((h00 / Da).astype(dtype) * ptcl0.disp +
+           (h10 / a3E0).astype(dtype) * ptcl0.vel +
+           (h01 / Da).astype(dtype) * ptcl1.disp +
+           (h11 / a3E1).astype(dtype) * ptcl1.vel)
+    vel *= (a**3 * jnp.sqrt(E2(a, cosmo))).astype(dtype)
 
     iptcl = Particles(ptcl0.conf, ptcl0.pmid, disp, vel=vel)
     return iptcl
