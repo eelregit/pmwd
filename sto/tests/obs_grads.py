f58e45f0bec5ca54b5bfbf98e6aeccdf34a14b93
Yucheng Zhang <yczhang.nyu@gmail.com>
2025-09-23 12:25:16 +0800

    WIP obs adjoint grad test

---
+++ sto/tests/obs_grads.py
@@ -0,0 +1,149 @@
+import os
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+from jax.lax import scan
+import matplotlib.pyplot as plt
+
+from pmwd import (
+    Configuration,
+    SimpleLCDM,
+    boltzmann,
+    white_noise, linear_modes,
+    lpt,
+    nbody,
+    scatter,
+)
+from pmwd.vis_util import simshow
+
+
+def gen_ic(modes, cosmo, conf):
+    modes = linear_modes(modes, cosmo, conf)
+    ptcl, _ = lpt(modes, cosmo, conf)
+    return ptcl
+
+
+def model(ptcl, cosmo, conf):  # we focus on nbody only
+    ptcl, obsvbl = nbody(ptcl, None, cosmo, conf)  # obsvbl init in nbody
+    def _scatter(carry, x):
+        ptcl = x
+        dens = scatter(ptcl, conf)
+        return None, dens
+    # make dens for all obsvbl snapshots
+    dens = scan(_scatter, None, obsvbl['snaps'])
+    return dens
+
+
+def obj(tgt_dens, ptcl, cosmo, conf):
+    dens = model(ptcl, cosmo, conf)
+    return (dens - tgt_dens).var()
+
+obj_grad = jax.grad(obj, argnums=(1, 2))
+
+
+ptcl_spacing = 1.
+ptcl_grid_shape = (128,) * 3
+conf = Configuration(ptcl_spacing, ptcl_grid_shape, mesh_shape=2,
+                     a_start=1/16, a_stop=1, a_nbody_num=16,
+                     a_snapshots=(0.5, 0.7, 0.9))  # set observable snapshots
+
+cosmo = SimpleLCDM(conf)
+cosmo = boltzmann(cosmo, conf)
+
+
+# control the target dens variation
+fname = 'dens.npy'
+if not os.path.exists(fname):
+    seed = 0  # seed for target
+    modes = white_noise(seed, conf)
+    ptcl = gen_ic(modes, cosmo, conf)
+
+    dens = model(ptcl, cosmo, conf)  # target density
+    jnp.save(fname, dens)
+dens = jnp.load(fname)
+
+
+# control the input modes variation
+fname = 'modes.npy'
+if not os.path.exists(fname):
+    seed = 1
+    modes = white_noise(seed, conf, real=True)
+    jnp.save(fname, modes)
+modes = jnp.load(fname)
+ptcl = gen_ic(modes, cosmo, conf)
+
+
+n = 64
+fname_am = 'grads_am{}.npy'  # adjoint mode gradients
+fname_ad = 'grads_ad{}.npy'  # AD mode gradients
+
+if not os.path.exists(fname_am.format(0)):  # adjoint gradients
+    print('#### adjoint method ####')
+    for i in range(n):
+        ptcl_grad, cosmo_grad = obj_grad(dens, ptcl, cosmo, conf)
+        jnp.save(fname_am.format(i), ptcl_grad.disp)
+        print(cosmo_grad)
+elif not os.path.exists(fname_ad.format(0)):  # AD gradients
+    # HACK for AD: commenting out custom_vjp and defvjp on scatter, gather, and nbody
+    print('#### AD ####')
+    for i in range(n):
+        ptcl_grad, cosmo_grad = obj_grad(dens, ptcl, cosmo, conf)
+        jnp.save(fname_ad.format(i), ptcl_grad.disp)
+        print(cosmo_grad)
+else:  # making plots
+    gam = np.stack([np.load(fname_am.format(i)) for i in range(n)], axis=0)
+    gad = np.stack([np.load(fname_ad.format(i)) for i in range(n)], axis=0)
+
+    from matplotlib.colors import SymLogNorm, LogNorm
+    plt.style.use('adjoint.mplstyle')
+
+    fig, _ = simshow(gam[0, 32], figsize=(3.5, 2.7), cmap='RdBu_r',
+                     norm=SymLogNorm(0.01, vmin=-0.1, vmax=0.1), colorbar=True,
+                     interpolation='none')
+    fig.savefig('obs_grads.pdf')
+    plt.close(fig)
+
+    fig, ax = plt.subplots(figsize=(2.5, 2.5))
+    bins = np.linspace(-0.32, 0.32, num=65, endpoint=True)
+    ax.hist2d(gam.ravel(), gad.ravel(), bins=[bins, bins], cmap='binary', norm=LogNorm())
+    ax.set_yticks(ax.get_xticks())
+    ax.set_xlim(bins[0], bins[-1])
+    ax.set_ylim(bins[0], bins[-1])
+    ax.set_xlabel('AD grad')
+    ax.set_ylabel('adjoint grad')
+    fig.savefig('obs_grads_cmp.pdf')
+    plt.close(fig)
+
+    def diffpair(g0, g1):
+        g0 = g0.reshape(n, np.prod(ptcl_grid_shape))
+        g1 = g1.reshape(n, np.prod(ptcl_grid_shape))
+        gd = np.zeros((n * (n-1) // 2, g0.shape[1]), dtype=g0.dtype)
+        for i in range(n):
+            for j in range(i):
+                ind  = i * (i-1) // 2 + j
+                gd[ind] = g1[j] - g0[i]
+        return gd.ravel()
+
+    fig, ax = plt.subplots(figsize=(2.5, 2.5))
+    bins = np.linspace(-6.4e-4, 6.4e-4, num=129, endpoint=True)
+    kwargs = dict(bins=bins, histtype='step', joinstyle='round',  capstyle='round')
+    gd = diffpair(gam, gam)
+    print('adj-adj:', gd.std())
+    ax.hist(gd, color='gray', ls='-', lw=1, label='adj-adj', **kwargs)
+    gd = diffpair(gam, gad)
+    print('adj-AD: ', gd.std())
+    ax.hist(gd, color='k', ls='-', lw=1, label='adj-AD', **kwargs)
+    gd = diffpair(gad, gad)
+    print('AD-AD:  ', gd.std())
+    ax.hist(gd, color='gray', ls=':', lw=1.5, label='AD-AD', **kwargs)
+    ax.set_xlim(bins[0], bins[-1])
+    ax.set_ylim(2e4, 5e9)
+    ax.set_yscale('log')
+    ax.ticklabel_format(axis='x', scilimits=(0, 0))
+    ax.tick_params(axis='y', which='minor', left=False, right=False)
+    ax.set_xlabel('grad diff')
+    ax.set_ylabel('counts')
+    ax.legend(loc=2, handlelength=0.5, handletextpad=0.5)
+    fig.savefig('obs_grads_diff.pdf')
+    plt.close(fig)
