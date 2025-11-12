4dceace2a77d196ae31eaffa1d69050627d99429
Yucheng Zhang <yczhang.nyu@gmail.com>
2025-11-12 12:15:14 +0800

    simplify so sharp.py

--- pmwd/sto/so/sharp.py
+++ pmwd/sto/so/sharp.py
@@ -6,48 +6,27 @@ from pmwd.sto.so.mlp import MLP
 from pmwd.sto.so.soft import soft_k, soft_kvec
 
 
-def apply_net(nid, conf, cosmo, x):
-    net = MLP(features=conf.so_nodes[nid])
-    return net.apply(cosmo.so_params[nid], x)
-
-
-def sonn_vmap(k, theta, cosmo, conf, nid):
-    """Evaluate the neural net, using vmap over k."""
-    def _sonn(_k):
-        _ft = soft_k(_k, theta)
-        return apply_net(nid, conf, cosmo, _ft)[0]
-    return vmap(_sonn)(k.ravel()).reshape(k.shape)
-
-
-def sonn_k(k, theta, cosmo, conf, nid):
-    """SO net of 1D k input."""
-    ft = soft_k( k, theta)
-    return apply_net(nid, conf, cosmo, ft)[..., 0]  # rm the trailing axis of dim one
-
-
-def sonn_kvec(kv, theta, cosmo, conf, nid):
-    """SO net of 3D k input, with permutation symmetry."""
-    kv = jnp.sort(kv, axis=-1)  # sort for permutation symmetry of kv components
-    ft = soft_kvec(kv, theta)
-    return apply_net(nid, conf, cosmo, ft)[..., 0]  # rm the trailing axis of dim one
-
-
 def pot_sharp(pot, kvec, theta, cosmo, conf, a):
     """SO of the laplace potential, function of 3D k vector (g function)."""
 
     if conf.so_type == 'NN' and conf.so_nodes[0] is not None:
-        # even function
-        kvec = map(jnp.abs, kvec)
+        kvec = map(jnp.abs, kvec)  # make even function of kvec
 
         # sparse to dense kvec, e.g. (128, 128, 65, 3)
         kv = jnp.stack(jnp.broadcast_arrays(*kvec), axis=-1)
 
-        @checkpoint  # checkpoint for saving memory in backward AD
-        def sonn_kvec_slice(k_):
-            return sonn_kvec(k_, theta, cosmo, conf, 0)
+        # sort for permutation symmetry of the spatial dimensions
+        kv = jnp.sort(kv, axis=-1)
 
-        # map for reduced memeory usage in the forward run
+        @checkpoint  # checkpoint for saving memory in backward AD
+        def sonn_kvec_slice(kv_):
+            ft = soft_kvec(kv_, theta)  # input features
+            mlp = MLP(features=conf.so_nodes[0])
+            g = mlp.apply(cosmo.so_params[0], ft)[..., 0]  # rm the trailing axis of dim one
+            return g
+        # map for reduced memory usage in the forward run
         g = jax.lax.map(sonn_kvec_slice, kv)
+
         pot *= g
 
     return pot
@@ -57,9 +36,12 @@ def grad_sharp(grad, k, theta, cosmo, conf, a):
     """SO of the gradient, function of 1D k component (f function)."""
 
     if conf.so_type == 'NN' and conf.so_nodes[1] is not None:
-        # make grad_sharp even function of k
-        k = jnp.abs(k)
-        f = sonn_k(k, theta, cosmo, conf, 1)
+        k = jnp.abs(k)  # make even function of k
+
+        ft = soft_k(k, theta)  # input features
+        mlp = MLP(features=conf.so_nodes[1])
+        f = mlp.apply(cosmo.so_params[1], ft)[..., 0]  # rm the trailing axis of dim one
+
         grad *= f
 
     return grad
