0de02b7ea538150a58fa196a24965a1c636a31a0
Yucheng Zhang <yczhang.nyu@gmail.com>
2026-01-05 17:01:24 +0800

    update loss functions, @jit on both disp_loss and dens_loss

--- pmwd/sto/train/loss.py
+++ pmwd/sto/train/loss.py
@@ -14,7 +14,10 @@ def snap_disp_loss(disp_d, disp_t):
 
 
 @jit
-def eval_disp_loss(disp, disp_t, box_size):
+def eval_disp_loss(obsvbl, tgts, box_size):
+    disp = obsvbl['snaps'].disp
+    disp_t = tgts[0]
+
     # get the relative disp
     disp_d = disp - disp_t
     # wrap to [-L/2, L/2] for the shorter disp
@@ -28,27 +31,33 @@ def eval_disp_loss(disp, disp_t, box_size):
     return loss
 
 
-def snap_dens_loss(snap, tgt, conf, offset, log_eps):
+def snap_dens_loss(disp, disp_t, pmid, conf, offset, log_eps):
     # get the density fields
-    dens = _scatter(snap.pmid, snap.disp, conf, None, None, offset, None)
-    dens_t = _scatter(snap.pmid, tgt[0], conf, None, None, offset, None)
+    dens = _scatter(pmid, disp, conf, None, None, offset, None)
+    dens_t = _scatter(pmid, disp_t, conf, None, None, offset, None)
 
     # loss on power spec
     k, P_d, _, _ = powspec(dens - dens_t, 1.)
     k, P_t, _, _ = powspec(dens_t, 1.)
     loss = jnp.sum(jnp.log(P_d / P_t + log_eps)) / len(k)
 
-    return loss.astype(conf.float_dtype)
+    return loss
+
+
+def _eval_dens_loss(conf, offset, log_eps, loss, x):
+    snap, disp_t = x
+    # accumulate loss of this snapshot
+    loss += snap_dens_loss(snap.disp, disp_t, snap.pmid, conf, offset, log_eps)
+    return loss, None
 
 
 @jit
-def eval_dens_loss(conf, offset, log_eps, loss, x):
-    tgt, snap = x
-
-    # accumulate loss of this snapshot
-    loss += snap_dens_loss(snap, tgt, conf, offset, log_eps)
-
-    return loss, None
+def eval_dens_loss(obsvbl, tgts, conf, offset, log_eps):
+    loss = 0.
+    # scan over snapshots to accumulate loss
+    loss, _ = scan(partial(_eval_dens_loss, conf, offset, log_eps),
+                   loss, (obsvbl['snaps'], tgts[0]))
+    return loss
 
 
 def loss_func(obsvbl, tgts, conf, loss_conf):
@@ -58,18 +67,12 @@ def loss_func(obsvbl, tgts, conf, loss_conf):
 
     if 'disp' in loss_conf['loss_fields']:
         box_size = jnp.array(conf.box_size, dtype=conf.float_dtype)
-        disp = obsvbl['snaps'].disp
-        disp_t = tgts[0].astype(conf.float_dtype)
-        loss += eval_disp_loss(disp, disp_t, box_size)
+        loss += eval_disp_loss(obsvbl, tgts, box_size).astype(conf.float_dtype)
 
     if 'dens' in loss_conf['loss_fields']:
-
-        offset = loss_conf['grid_offset']
-        log_eps = loss_conf['log_eps']
-
-        # scan over snapshots to accumulate loss
-        loss, _ = scan(partial(eval_dens_loss, conf, offset, log_eps),
-                       loss, (tgts, obsvbl['snaps']))
+        offset = jnp.array(loss_conf['grid_offset'], dtype=conf.float_dtype)
+        log_eps = jnp.array(loss_conf['log_eps'], dtype=conf.float_dtype)
+        loss += eval_dens_loss(obsvbl, tgts, conf, offset, log_eps).astype(conf.float_dtype)
 
     # mean loss per snapshot
     loss /= n_snaps
